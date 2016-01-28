8815df85bd4671ee71854f9687919f086240c281
Leslie Emery <emeryl@uw.edu>
2016-01-28 15:59:52 -0800

    Use bootstrap classes for django-tables2
    Clean up the trait_table.html template and add bootstrap classes to the
    Meta.attrs of the SourceTraitTable object. Also remove an extraneous line
    from settings.py where django.template.context_processors.request was
    included twice in the context_processors list of TEMPLATES.

--- trait_browser/tables.py
+++ trait_browser/tables.py
@@ -4,7 +4,10 @@ from .models import SourceTrait
 # Define the table classes for displaying nice HTML tables with django-tables2
 class SourceTraitTable(tables.Table):
     name = tables.LinkColumn('trait_browser_source_trait_detail', args=[tables.utils.A('pk')])
+    # 
     
     class Meta:
         model = SourceTrait
-        attrs = {'class': 'traitTable'}
+        fields = ('name', 'description', 'study_name', )
+        attrs = {'class': 'table table-striped table-bordered table-hover', 'template': 'django_tables2/bootstrap.html'}
+        order_by = ('name', 'study_name', )
