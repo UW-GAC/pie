452a54ef7adef1d3e7e7f07ee483037e9d2ddaf5
Leslie Emery <emeryl@uw.edu>
2016-01-28 17:16:11 -0800

    Style for SourceTraitTable
    Switch to using a custom template for the SourceTraitTable class. Create that
    custom template (copy from the bootstrap.html included with django-tables2).
    Add custom CSS to style.css for displaying glyphicons as the sorting arrows
    for sorting SourceTraitTables. Move verbose_names to the SourceTraitTable
    class (previously in the model fields). Turn off sorting for description field.

--- trait_browser/tables.py
+++ trait_browser/tables.py
@@ -3,11 +3,13 @@ from .models import SourceTrait
 
 # Define the table classes for displaying nice HTML tables with django-tables2
 class SourceTraitTable(tables.Table):
-    name = tables.LinkColumn('trait_browser_source_trait_detail', args=[tables.utils.A('pk')])
+    name = tables.LinkColumn('trait_browser_source_trait_detail', args=[tables.utils.A('pk')], verbose_name='Trait name')
+    description = tables.Column('Trait description', orderable=False)
+    study_name = tables.Column('Study name')
     # 
     
     class Meta:
         model = SourceTrait
         fields = ('name', 'description', 'study_name', )
-        attrs = {'class': 'table table-striped table-bordered table-hover', 'template': 'django_tables2/bootstrap.html'}
+        attrs = {'class': 'table table-striped table-bordered table-hover', 'template': 'trait_browser/bootstrap_tables2.html'}
         order_by = ('name', 'study_name', )
