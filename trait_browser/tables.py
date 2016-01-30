835730538e2d78a200165a557e64b0efba4cb818
Adrienne Stilp <amstilp@uw.edu>
2016-01-29 16:23:52 -0800

    Fix study name in table now that the study field is a foreign key

--- trait_browser/tables.py
+++ trait_browser/tables.py
@@ -3,14 +3,15 @@ from .models import SourceTrait
 
 # Define the table classes for displaying nice HTML tables with django-tables2
 class SourceTraitTable(tables.Table):
+    # in this section, define the variables referenced below in Meta
     name = tables.LinkColumn('trait_browser_source_trait_detail', args=[tables.utils.A('pk')], verbose_name='Trait name')
     description = tables.Column('Trait description', orderable=False)
-    study_name = tables.Column('Study name')
-    # 
+    # grab the name from the Study linked to this trait
+    study_name = tables.Column('Study name', accessor='study.name')
     
     class Meta:
         model = SourceTrait
-        fields = ('name', 'description', 'study_name', )
+        fields = ('name', 'description', 'study_name')
         attrs = {'class': 'table table-striped table-bordered table-hover'}
         template = 'trait_browser/bootstrap_tables2.html'
         order_by = ('name', 'study_name', )
