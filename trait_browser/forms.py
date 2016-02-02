f14b8668d6511372bc4e5868f14150cada52a265
Adrienne Stilp <amstilp@uw.edu>
2016-02-02 08:04:54 -0800

    remove extra div for padding as it's no longer necessary

--- trait_browser/forms.py
+++ trait_browser/forms.py
@@ -36,14 +36,12 @@ class SourceTraitCrispySearchForm(forms.Form):
     </a>
     <div class="collapse" id="traitStudyFilterField">
         <div class="well">
-            <div class="col-lg-2">
         """),
    
         InlineCheckboxes('study'),
         # end the collapsible divs
         # the <hr> adds some space between this button and the submit button
         HTML("""
-                </div>
               </div>
             </div>
             <hr>
