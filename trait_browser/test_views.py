1162d4deaa811f28535ad1e36e5d600aad23746a
Leslie Emery <emeryl@uw.edu>
2016-02-10 11:23:49 -0800

    Revert "Implement model_mommy for generating test model values"
    This reverts commit 70bce087b3f12949079d11f76c25d72b17396c27.

--- trait_browser/test_views.py
+++ trait_browser/test_views.py
@@ -1,12 +1,8 @@
-from model_mommy import mommy
-
 from django.test import TestCase
 from django.core.urlresolvers import reverse
-
 from .models import SourceEncodedValue, SourceTrait, Study
 from .tables import SourceTraitTable
 from .views import TABLE_PER_PAGE, search
-
 
 def create_study(name='Some Study', study_id=1):
     """Create a simple Study object, for testing views.
@@ -36,8 +32,9 @@ class TraitBrowserSearchTestCase(TestCase):
         """Test that the search function finds an exact match in the SourceTrait
         name field, but doesn't find a non-match."""
         # Set up study and SourceTrait objects
-        st_match = mommy.make(SourceTrait, name='foo_bar', dcc_trait_id=1)
-        st_nonmatch = mommy.make(SourceTrait, name='sum_es', dcc_trait_id=2)
+        study = create_study()
+        st_match = create_source_trait(name='foo_bar', dcc_trait_id=1)
+        st_nonmatch = create_source_trait(name='sum_es', dcc_trait_id=2)
         # Get the search results
         search1 = search('foo_bar', 'source')
         # Check that the matching trait is found, but the non-match is not
@@ -48,14 +45,9 @@ class TraitBrowserSearchTestCase(TestCase):
         """Test that the search function finds a substring match in the SourceTrait
         name field, but doesn't find a non-match."""
         # Set up study and SourceTrait objects
-        st_match = mommy.make(SourceTrait,
-                              name='foo_bar',
-                              description='',
-                              dcc_trait_id=1)
-        st_nonmatch = mommy.make(SourceTrait,
-                                 name='sum_es',
-                                 description='',
-                                 dcc_trait_id=2)
+        study = create_study()
+        st_match = create_source_trait(name='foo_bar', dcc_trait_id=1)
+        st_nonmatch = create_source_trait(name='sum_es', dcc_trait_id=2)
         # Get the search results
         search1 = search('bar', 'source')
         # Check that the matching trait is found, but the non-match is not
@@ -66,14 +58,9 @@ class TraitBrowserSearchTestCase(TestCase):
         """Test that the search function finds an exact match in the SourceTrait
         description field, but doesn't find a non-match."""
         # Set up study and SourceTrait objects
-        st_match = mommy.make(SourceTrait,
-                              name='baz',
-                              description='foo and bar',
-                              dcc_trait_id=1)
-        st_nonmatch = mommy.make(SourceTrait,
-                                 name='spam',
-                                 description='sum and es',
-                                 dcc_trait_id=2)
+        study = create_study()
+        st_match = create_source_trait(description='foo and bar', dcc_trait_id=1)
+        st_nonmatch = create_source_trait(description='sum and es', dcc_trait_id=2)
         # Get the search results
         search1 = search('foo and bar', 'source')
         # Check that the matching trait is found, but the non-match is not
@@ -84,8 +71,9 @@ class TraitBrowserSearchTestCase(TestCase):
         """Test that the search function finds a substring match in the SourceTrait
         description field, but doesn't find a non-match."""
         # Set up study and SourceTrait objects
-        st_match = mommy.make(SourceTrait, description='foo and bar', dcc_trait_id=1)
-        st_nonmatch = mommy.make(SourceTrait, description='sum and es', dcc_trait_id=2)
+        study = create_study()
+        st_match = create_source_trait(description='foo and bar', dcc_trait_id=1)
+        st_nonmatch = create_source_trait(description='sum and es', dcc_trait_id=2)
         # Get the search results
         search1 = search('bar', 'source')
         # Check that the matching trait is found, but the non-match is not
@@ -96,10 +84,12 @@ class TraitBrowserSearchTestCase(TestCase):
         """Test that the search function finds a matching name in one particular
         study, but doesn't find a match from a different study. """
         # Set up study and SourceTrait objects
-        st_match = mommy.make(SourceTrait, name='foo_bar', dcc_trait_id=1,
-                              study__study_id=1)
-        st_nonmatch = mommy.make(SourceTrait, name='foo_bar', dcc_trait_id=2,
-                                 study__study_id=2)
+        study1 = create_study(study_id=1, name='Study 1')
+        study2 = create_study(study_id=2, name='Study 2')
+        st_match = create_source_trait(name='foo_bar', dcc_trait_id=1,
+                                       study=Study.objects.get(study_id=1))
+        st_nonmatch = create_source_trait(name='foo_bar', dcc_trait_id=2,
+                                          study=Study.objects.get(study_id=2))
         # Get the search results
         search1 = search('bar', 'source', studies=[(1, 'Study 1')])
         # Check that the matching trait is found, but the non-match is not
@@ -128,8 +118,10 @@ class TraitBrowserViewsTestCase(TestCase):
         """Tests that the source_trait_table view works with fewer rows than
         will require a second page."""
         # Make less than one page of SourceTraits
+        create_study()
         n_traits = TABLE_PER_PAGE - 2
-        mommy.make(SourceTrait, _quantity=n_traits)
+        for n in range(n_traits):
+            create_source_trait(dcc_trait_id=n+1)
         # Test the view
         url = reverse('trait_browser_source_trait_table')
         response = self.client.get(url)
@@ -142,8 +134,10 @@ class TraitBrowserViewsTestCase(TestCase):
         """Tests that the source_trait_table view works with two pages' worth of
         rows."""
         # Make less than one page of SourceTraits
+        create_study()
         n_traits = TABLE_PER_PAGE * 2
-        mommy.make(SourceTrait, _quantity=n_traits)
+        for n in range(n_traits):
+            create_source_trait(dcc_trait_id=n+1)
         # Test the view
         url = reverse('trait_browser_source_trait_table')
         response = self.client.get(url)
@@ -151,13 +145,16 @@ class TraitBrowserViewsTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         # Does the source trait table object have n_traits rows
         self.assertEqual(len(response.context['trait_table'].rows), n_traits)
-    
+
     def test_source_trait_detail_valid(self):
         """Tests that the SourceTrait detail page returns 200 with a valid pk"""
         # Set up a valid study and source trait
-        st = mommy.make(SourceTrait, phv_string='phv1034')
+        create_study()
+        create_source_trait()
+        # Get the pk of a valid SourceTrait
+        good_pk = SourceTrait.objects.all()[0].dcc_trait_id
         # Test that the page works with a valid pk
-        url = reverse('trait_browser_source_trait_detail', args=[st.dcc_trait_id])
+        url = reverse('trait_browser_source_trait_detail', args=[good_pk])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         
