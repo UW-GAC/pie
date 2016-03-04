f6127a3ba57eea48f538ee70967f51fc41c08582
Leslie Emery <emeryl@uw.edu>
2016-03-04 15:42:59 -0800

    Move class Meta after fields declaration in models.
    This change is according to Django style guide.

--- trait_browser/factories.py
+++ trait_browser/factories.py
@@ -12,22 +12,18 @@ from .models import Study, SourceTrait, SourceEncodedValue
 
 class StudyFactory(factory.DjangoModelFactory):
     """Factory for Study objects using Faker faked data."""
-    
-    class Meta:
-        model = Study
-        django_get_or_create = ('name', 'study_id', )
         
     name = factory.Faker('company')
     study_id = factory.Sequence(lambda n: n)
     dbgap_id = factory.Sequence(lambda n: 'phs{0}'.format(n))
+    
+    class Meta:
+        model = Study
+        django_get_or_create = ('name', 'study_id', )
 
 
 class SourceTraitFactory(factory.DjangoModelFactory):
     """Factory for SourceTrait objects using Faker faked data."""
-    
-    class Meta:
-        model = SourceTrait
-        django_get_or_create = ('dcc_trait_id', )
     
     dcc_trait_id = factory.Sequence(lambda n: n)
     name = factory.Faker('word')
@@ -38,15 +34,19 @@ class SourceTraitFactory(factory.DjangoModelFactory):
     phs_string = factory.Sequence(lambda n: 'phs{0}'.format(n))
     phv_string = factory.Sequence(lambda n: 'phv{0}'.format(n))
     study = factory.SubFactory(StudyFactory)
+    
+    class Meta:
+        model = SourceTrait
+        django_get_or_create = ('dcc_trait_id', )
 
 
 class SourceEncodedValueFactory(factory.DjangoModelFactory):
     """Factory for SourceEncodedValue objects using Faker faked data."""
     
-    class Meta:
-        model = SourceEncodedValue
-    
     category = factory.Faker('word')
     value = factory.Faker('text', max_nb_chars=50)
     web_date_added = factory.fuzzy.FuzzyDateTime(start_dt=timezone.make_aware(datetime(2016, 1, 1)))
-    source_trait = factory.SubFactory(SourceTraitFactory)
+    source_trait = factory.SubFactory(SourceTraitFactory) 
+       
+    class Meta:
+        model = SourceEncodedValue
