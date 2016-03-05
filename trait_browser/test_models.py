9aff6e9cd420c9bb78db3bd6f1d9195c19fb1beb
Leslie Emery <emeryl@uw.edu>
2016-03-04 16:32:37 -0800

    Implement tests that catch errors in two overlooked functions.
    SourceEncodedValue.get_source_trait_name() and
    SourceEncodedValue.get_source_trait_study() were not previously covered in tests
    and this resulted in an undetected error. This error only shows up in the admin
    interface and prevents the SourceEncodedValues admin page from displaying. One of
    the new tests catches this error, and the other will catch errors in another
    related function that was also overlooked.

--- trait_browser/test_models.py
+++ trait_browser/test_models.py
@@ -74,3 +74,13 @@ class SourceTraitEncodedValueTestCase(TestCase):
         """Ensure that the custom printing method works."""
         enc_value = SourceEncodedValueFactory.build()
         self.assertIsInstance(enc_value.__str__(), str)
+        
+    def test_get_source_trait_name(self):
+        """Ensure that get_source_trait_name() works."""
+        enc_value = SourceEncodedValueFactory.build()
+        enc_value.get_source_trait_name()
+        
+    def test_get_source_trait_enc_value(self):
+        """Ensure that SourceEncodedValue.get_source_trait_enc_value() works."""
+        enc_value = SourceEncodedValueFactory.build()
+        enc_value.get_source_trait_study()
