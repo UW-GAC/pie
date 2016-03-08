bed32af69b45a0f37392dad941e327d245e5fdb9
Leslie Emery <emeryl@uw.edu>
2016-03-08 10:17:38 -0800

    Fix unresolved merge conflict that I missed in top level urls.py
    I was working out how to use opendiff as a mergetool and thought I'd
    resolved this set of merge conflicts, but I hadn't. Now these are
    fixed.

--- phenotype_inventory/urls.py
+++ phenotype_inventory/urls.py
@@ -20,21 +20,12 @@ from django.contrib import admin
 
 
 urlpatterns = [
-<<<<<<< HEAD
-    url(r'^home/', TemplateView.as_view(template_name='home.html')),    # static home page
-    url(r'^about/', TemplateView.as_view(template_name='about.html')),    # static about page
-    url(r'^contact/', TemplateView.as_view(template_name='contact.html')),    # static contact page
-    url(r'^pages/', include('django.contrib.flatpages.urls')),    # Flat pages
+    url(r'^home/', TemplateView.as_view(template_name="home.html"), name='home'),    # static home page
+    url(r'^about/', TemplateView.as_view(template_name="about.html"), name='about'),    # static home page
+    url(r'^contact/', TemplateView.as_view(template_name="contact.html"), name='contact'),    # static home page
+    url(r'^pages/', include('django.contrib.flatpages.urls')),    # Flat pages 
     url(r'^admin/', include(admin.site.urls)),    # Admin interface
     url(r'^trait_browser/', include('trait_browser.urls')),    # Trait browser app
-=======
-    url(r'^home/', TemplateView.as_view(template_name="home.html"), name='home'), # static home page
-    url(r'^about/', TemplateView.as_view(template_name="about.html"), name='about'), # static home page
-    url(r'^contact/', TemplateView.as_view(template_name="contact.html"), name='contact'), # static home page
-    url(r'^pages/', include('django.contrib.flatpages.urls')),   # Flat pages 
-    url(r'^admin/', include(admin.site.urls)),                   # Admin interface
-    url(r'^trait_browser/', include('trait_browser.urls')),      # Trait browser app
->>>>>>> hotfix/apache-nonroot
 ]
 
 # Set the name for the admin site.
