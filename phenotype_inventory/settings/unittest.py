b0e7399c135e78da699c2b8f01839977959dd1b8
Leslie Emery <emeryl@uw.edu>
2016-03-17 10:48:49 -0700

    Add unittest settings module since tests stopped working with MariaDB
    The topmed user doesn't have database create privileges, and the
    unit tests create a new database for each test function, so the
    tests currently fail because Django can't create a test database.
    This new settings module can be used to run tests against a SQLite
    database in the meantime.

---
+++ phenotype_inventory/settings/unittest.py
@@ -0,0 +1,41 @@
+"""Settings to run unittests on servers for the phenotype_inventory project.
+
+Use this settings module only with the migrate.py test command. Make sure to
+manually specify this settings module, like so:
+
+./manage.py test --settings=phenotype_inventory.settings.unittest
+
+DEBUG is set to True
+The database backend is SQLite
+SECRET_KEY and CNF_PATH are obtained from the .secrets.json file.
+"""
+
+import os
+
+from .base import *
+from .staging import secrets, get_secret
+
+
+# DEBUG SETTINGS
+DEBUG = True
+
+
+# DATABASE SETTINGS
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.normpath(os.path.join(SITE_ROOT, 'site_db.sqlite3')),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+
+# SNUFFLES DATABASE CONNECTION SETTINGS
+CNF_PATH = get_secret('CNF_PATH')
+
+
+# SECRET_KEY SETTINGS
+SECRET_KEY = get_secret('DJANGO_SECRET_KEY')
