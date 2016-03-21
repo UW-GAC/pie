d6d10398436e330589b80e3203f3a4c7319e9f56
Leslie Emery <emeryl@uw.edu>
2016-03-21 13:51:01 -0700

    Fix indentation alignment in trait_browser.urls
    I got annoyed with the way I had indented and aligned these
    urlpattern URLs. I fixed it.

--- trait_browser/urls.py
+++ trait_browser/urls.py
@@ -10,21 +10,12 @@ from . import views
 
 
 urlpatterns = [
-    url(
-        r'^source_trait/(?P<pk>\d+)/detail/$',
-        views.SourceTraitDetail.as_view(),
-        name='trait_browser_source_trait_detail'
-    ),
-    url(
-        r'^table/source_trait/',
-        views.source_trait_table,
-        name='trait_browser_source_trait_table'
-    ),
-    url(
-        r'^search/source_trait/',
-        views.source_trait_search,
-        name='trait_browser_source_trait_search'
-    ),
+    url(r'^source_trait/(?P<pk>\d+)/detail/$', views.SourceTraitDetail.as_view(),
+        name='trait_browser_source_trait_detail'),
+    url(r'^table/source_trait/', views.source_trait_table,
+        name='trait_browser_source_trait_table'),
+    url(r'^search/source_trait/', views.source_trait_search,
+        name='trait_browser_source_trait_search'),
     url(r'^source_trait/study/(?P<pk>\d+)/$', views.study_source_trait_table,
         name='trait_browser_study_source_trait_table')
 ]
