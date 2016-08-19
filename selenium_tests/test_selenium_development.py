d0e1e9ffc653dfc75db46d56e11f7c15630d77b3
Leslie Emery <emeryl@uw.edu>
2016-08-19 13:54:14 -0700

    WIP from when I was working on this in March - unknown purpose
    Commit some changes to the test_selenium_development.py module, the
    purpose of which I don't recall. I was in the middle of working on this
    in March, but stopped before finishing the goal. I don't remember why
    I implemented these changes now and I don't want to lose them. I'll undo
    them in the next commit.

--- selenium_tests/test_selenium_development.py
+++ selenium_tests/test_selenium_development.py
@@ -3,9 +3,9 @@
 from contextlib import contextmanager
 from os import environ
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.expected_conditions import staleness_of
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 import re
 import time
 
@@ -57,7 +57,7 @@ class SeleniumTestCase(StaticLiveServerTestCase):
     def tearDownClass(cls):
         cls.selenium.quit()
         super(SeleniumTestCase, cls).tearDownClass()
-        
+    
     def get_reverse(self, view_name, *args):
         """Use selenium to get the URL for the given reversed view_name URL."""
         self.selenium.get(self.live_server_url + reverse(view_name, args=args))
@@ -76,7 +76,8 @@ class SeleniumTestCase(StaticLiveServerTestCase):
     
     def click_next_button(self):
         """Click on the first link with text 'Next'."""
-        self.selenium.find_element_by_link_text('Next').click()
+        with self.wait_for_page_load():
+            self.selenium.find_element_by_link_text('Next').click()
     
     def click_previous_button(self):
         """Click on the first link with text 'Previous'."""
