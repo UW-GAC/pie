e888f6f32bd55b514ff09e287b2bf03d58747c4d
Leslie Emery <emeryl@uw.edu>
2016-08-29 16:55:44 -0700

    Add SourceStudyVersion factory.

--- trait_browser/factories.py
+++ trait_browser/factories.py
@@ -12,6 +12,14 @@ from .models import (GlobalStudy, Study, SourceStudyVersion, Subcohort,
                      SourceDataset, SourceDatasetSubcohorts, SourceDatasetUniqueKeys, HarmonizedTraitSet,
                      SourceTrait, HarmonizedTrait, SourceTraitEncodedValue, HarmonizedTraitEncodedValue)
 
+# Use these later for SourceStudyVersion factories.
+VISIT_CHOICES = ("one_visit_per_file",
+                 "visit_as_variable",
+                 "event_data",
+                 "not_associated_with_specific_visit",
+                 "visit_in_variable_descriptions",
+                 "complicated",
+                 "missing_visit_information")
 
 class GlobalStudyFactory(factor.DjangoModelFactory):
     """Factory for GlobalStudy objects using Faker faked data."""
@@ -36,6 +44,25 @@ class StudyFactory(factory.DjangoModelFactory):
     class Meta:
         model = Study
         django_get_or_create = ('i_accession', 'i_study_name', )
+
+
+class SourceStudyVersionFactory(factory.DjangoModelFactory):
+    """Factory for SourceStudyVersion objecsts using Faker faked data."""
+    
+    study = factory.SubFactory(Study)
+    i_id = factory.Sequence(lambda n: n)
+    i_accession = randint(1, 999999)
+    i_version = randint(1, 10)
+    i_is_subject_file = factory.Faker('boolean')
+    i_study_subject_column = factory.Faker('pystr', max_chars=45)
+    i_is_medication_dataset = factory.Faker('boolean')
+    i_dbgap_description = factory.Faker('text')
+    i_dcc_description = factory.Faker('text')
+    web_date_added = factory.fuzzy.FuzzyDateTime(start_dt=timezone.make_aware(datetime(2016, 1, 1)))    
+    
+    class Meta:
+        model = SourceStudyVersion
+        django_get_or_create = ('i_id', )
 
 
 class SourceTraitFactory(factory.DjangoModelFactory):
