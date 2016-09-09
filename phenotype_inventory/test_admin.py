cdf47a999b593d59c1e9c4c22d38c4646b57584f
Leslie Emery <emeryl@uw.edu>
2016-09-09 14:59:30 -0700

    Change test_admin.py to use ViewsAutoLoginTestCase - still not working though.

--- phenotype_inventory/test_admin.py
+++ phenotype_inventory/test_admin.py
@@ -1,10 +1,9 @@
 """Test the functionality of the admin interface for the entire phenotype_inventory project."""
 
-from django.test import TestCase
 from django.core.urlresolvers import reverse
+from trait_browser.test_views import ViewsAutoLoginTestCase
 
-
-class AdminTestCase(TestCase):
+class AdminTestCase(ViewsAutoLoginTestCase):
     """Unit tests for the views about source traits."""
     
     def test_admin_exists(self):
@@ -12,6 +11,6 @@ class AdminTestCase(TestCase):
         url = reverse('admin:index')
         response = self.client.get(url)
         # Does the URL work?
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
         # This gives a 302 (redirect) rather than a 200 code, because it
         # redirects you to the login page. I'll change this later.
