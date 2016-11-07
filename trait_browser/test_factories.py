43288676c70fc5e5dfed5f6f848b5ad111516069
Leslie Emery <emeryl@uw.edu>
2016-11-07 15:22:06 -0800

    Change tests of build_test_db for the addition of harmonized traits.

--- trait_browser/test_factories.py
+++ trait_browser/test_factories.py
@@ -285,14 +285,31 @@ class HarmonizedTraitEncodedValueFactoryTestCase(TestCase):
 
 class BuildTestDbTestCase(TestCase):
     
-    def test_build_db_error(self):
+    def test_build_db_global_studies_error(self):
         """Test that calling build_test_db() with too small a value for n_global_studies raises ValueError."""
         with self.assertRaises(ValueError):
-            build_test_db(1, (2,3), (3,9), (2,16), (2,9))    
+            build_test_db(n_global_studies=1,
+                          n_subcohort_range=(2,3),
+                          n_dataset_range=(3,9),
+                          n_trait_range=(2,16),
+                          n_enc_value_range=(2,9))    
     
+    def test_build_db_trait_range_error(self):
+        """Test that calling build_test_db() with too small a value for n_global_studies raises ValueError."""
+        with self.assertRaises(ValueError):
+            build_test_db(n_global_studies=3,
+                          n_subcohort_range=(2,3),
+                          n_dataset_range=(3,9),
+                          n_trait_range=(22,16),
+                          n_enc_value_range=(2,9))    
+
     def test_build_db1(self):
         """Test that building a db of fake data works. Run the same test several times with different values."""
-        build_test_db(3, (2,3), (3,9), (2,16), (2,9))
+        build_test_db(n_global_studies=3,
+                      n_subcohort_range=(2,3),
+                      n_dataset_range=(3,9),
+                      n_trait_range=(2,16),
+                      n_enc_value_range=(2,9))
         # Make sure there are saved objects for each of the models.
         self.assertTrue(GlobalStudy.objects.count() > 0)
         self.assertTrue(Study.objects.count() > 0)
@@ -304,7 +321,11 @@ class BuildTestDbTestCase(TestCase):
 
     def test_build_db2(self):
         """Test that building a db of fake data works. Run the same test several times with different values."""
-        build_test_db(10, (2,3), (3,9), (2,16), (2,9))
+        build_test_db(n_global_studies=10,
+                      n_subcohort_range=(2,3),
+                      n_dataset_range=(3,9),
+                      n_trait_range=(2,16),
+                      n_enc_value_range=(2,9))
         # Make sure there are saved objects for each of the models.
         self.assertTrue(GlobalStudy.objects.count() > 0)
         self.assertTrue(Study.objects.count() > 0)
@@ -316,7 +337,11 @@ class BuildTestDbTestCase(TestCase):
 
     def test_build_db3(self):
         """Test that building a db of fake data works. Run the same test several times with different values."""
-        build_test_db(3, (1,2), (1,2), (1,2), (1,2))
+        build_test_db(n_global_studies=3,
+                      n_subcohort_range=(1,2),
+                      n_dataset_range=(1,2),
+                      n_trait_range=(2,3),
+                      n_enc_value_range=(1,2))
         # Make sure there are saved objects for each of the models.
         self.assertTrue(GlobalStudy.objects.count() > 0)
         self.assertTrue(Study.objects.count() > 0)
