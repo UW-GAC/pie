acec5ff384244d085e8dd5b944ed82f44ab962e6
Leslie Emery <emeryl@uw.edu>
2016-12-23 18:36:35 -0800

    Organize recipes/urls.py into unit_patterns and harmonization_patterns with include

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,20 +4,24 @@ These urlpatterns are included in the project's urlpatterns, so these
 urls will show up under /recipe.
 """
 
-from django.conf.urls import url
+from django.conf.urls import include, url
 
 from . import views
 
+unit_patterns = [
+    url(r'^new/$', views.CreateUnitRecipe.as_view(), name='new_unit'),
+    url(r'^edit/(?P<pk>\d+)/$', views.UpdateUnitRecipe.as_view(), name='edit_unit'),
+    url(r'^detail/(?P<pk>\d+)/$', views.UnitRecipeDetail.as_view(), name='unit_detail'),    
+    url(r'^unit/autocomplete/$', views.UnitRecipeIDAutocomplete.as_view(), name='unit_autocomplete'),
+]
+
+harmonization_patterns = [
+    url(r'^new/$', views.CreateHarmonizationRecipe.as_view(), name='new_harmonization'),
+    url(r'^edit/(?P<pk>\d+)/$', views.UpdateHarmonizationRecipe.as_view(), name='edit_harmonization'),
+    url(r'^detail/(?P<pk>\d+)/$', views.HarmonizationRecipeDetail.as_view(), name='harmonization_detail'),
+]
 
 urlpatterns = [
-    # Request form views
-    url(r'^unit/new/$', views.CreateUnitRecipe.as_view(), name='new_unit'),
-    url(r'^harmonization/new/$', views.CreateHarmonizationRecipe.as_view(), name='new_harmonization'),
-    url(r'^unit/edit/(?P<pk>\d+)/$', views.UpdateUnitRecipe.as_view(), name='edit_unit'),
-    url(r'^harmonization/edit/(?P<pk>\d+)/$', views.UpdateHarmonizationRecipe.as_view(), name='edit_harmonization'),
-    # Autocomplete views
-    url(r'^unit/autocomplete/$', views.UnitRecipeIDAutocomplete.as_view(), name='unit_autocomplete'),
-    # Detail views
-    url(r'^unit/detail/(?P<pk>\d+)/$', views.UnitRecipeDetail.as_view(), name='unit_detail'),
-    url(r'^harmonization/detail/(?P<pk>\d+)/$', views.HarmonizationRecipeDetail.as_view(), name='harmonization_detail'),
+    url(r'^unit/', include(unit_patterns)),
+    url(r'^harmonization/', include(harmonization_patterns)),
 ]
