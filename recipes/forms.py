038c04ece93ae69a804978a407321baa87f39514
Leslie Emery <emeryl@uw.edu>
2016-12-27 14:32:42 -0800

    Add help_text to all fields in UnitRecipeForm and HarmonizationRecipeForm

--- recipes/forms.py
+++ recipes/forms.py
@@ -21,9 +21,15 @@ class UnitRecipeForm(forms.ModelForm):
         model = UnitRecipe
         fields = ('name', 'age_variables', 'batch_variables', 'phenotype_variables', 'instructions', )
         widgets = {'age_variables': autocomplete.ModelSelect2Multiple(url='trait_browser:source_autocomplete'),
-                   'batch_variables': autocomplete.ModelSelect2Multiple(url='trait_browser:source_autocomplete'),
-                   'phenotype_variables': autocomplete.ModelSelect2Multiple(url='trait_browser:source_autocomplete'),
-                   }
+            'batch_variables': autocomplete.ModelSelect2Multiple(url='trait_browser:source_autocomplete'),
+            'phenotype_variables': autocomplete.ModelSelect2Multiple(url='trait_browser:source_autocomplete'),
+        }
+        help_texts = {'name': 'A unique and informative name for the harmonization unit.',
+            'age_variables': 'Enter the DCC id(s) of the variable(s) needed to derive age for your harmonized variable <strong>in this harmonization unit</strong>.',
+            'batch_variables': 'Enter the DCC id(s) of the variable(s) needed to derive a harmonized batch variable <strong>in this harmonization unit</strong>. (optional)',
+            'phenotype_variables': 'Enter the DCC id(s) of the variable(s) needed to derive your target harmonized variable <strong>in this harmonization unit</strong>.',
+            'instructions': 'Describe how to use the age variables to derive age, how to use the batch variables to derive a harmonized batch, and how to use the phenotype variables to derive your target harmonized variable <strong>in this harmonization unit</strong>.',
+        }
 
     def get_model_name(self):
         """ """
@@ -42,6 +48,15 @@ class HarmonizationRecipeForm(forms.ModelForm):
         model = HarmonizationRecipe
         fields = ('name', 'target_name', 'target_description', 'measurement_unit', 'type', 'units', 'encoded_values', )
         # widgets = {'units': autocomplete.ModelSelect2Multiple(url='recipes:unit_autocomplete'), }
+        help_texts = {
+            'name': 'A unique and informative name for your harmonization recipe.',
+            'target_name': 'A short and informative name for your harmonized variable. Only letters, numbers, and underscores allowed.',
+            'target_description': 'A detailed description of your harmonized variable. This description will appear in documentation and data dictionaries.',
+            'measurement_unit': 'The units of measurement for your target harmonized variable.',
+            'type': 'The general type of harmonization required to produce your target variable.',
+            'units': 'The harmonization units to include in your target harmonized variable.',
+            'encoded_values': 'Values and descriptions for encoded values for your target harmonized variable. Define one encoded value per line, separating the value from its description with a semicolon and a single space.<br>Example:<br>1: blue<br>2: red<br>3: green',
+        }
     
     def get_model_name(self):
         """ """
