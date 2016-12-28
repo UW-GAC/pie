7f84bb70ced90b089ae9680c55d08f1257db95f2
Leslie Emery <emeryl@uw.edu>
2016-12-27 18:13:37 -0800

    Make initial migrations for trait_browser and recipes apps

---
+++ recipes/migrations/0001_initial.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('trait_browser', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='HarmonizationRecipe',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+                ('name', models.CharField(verbose_name='harmonization recipe name', max_length=1000)),
+                ('version', models.IntegerField(default=1)),
+                ('target_name', models.CharField(verbose_name='target phenotype variable name', max_length=50)),
+                ('target_description', models.CharField(verbose_name='target phenotype variable description', max_length=1000)),
+                ('encoded_values', models.TextField(blank=True, verbose_name='definition of encoded values for target variable')),
+                ('type', models.CharField(verbose_name='harmonization type', max_length=100, choices=[('unit_recode', 'recode variable for different units of measurement'), ('category_recode', 'recode variable for new encoded value category definitions'), ('formula', 'calculate variable from a formula'), ('other', 'other')])),
+                ('measurement_unit', models.CharField(verbose_name='unit of measurement', max_length=100)),
+                ('creator', models.ForeignKey(related_name='harmonization_recipes_created_by', to=settings.AUTH_USER_MODEL)),
+                ('last_modifier', models.ForeignKey(related_name='harmonization_recipes_last_modified_by', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='UnitRecipe',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+                ('instructions', models.TextField(verbose_name='harmonization instructions')),
+                ('version', models.IntegerField(default=1)),
+                ('name', models.CharField(verbose_name='harmonization unit name', max_length=1000)),
+                ('age_variables', models.ManyToManyField(related_name='units_as_age_trait', to='trait_browser.SourceTrait')),
+                ('batch_variables', models.ManyToManyField(related_name='units_as_batch_trait', to='trait_browser.SourceTrait')),
+                ('creator', models.ForeignKey(related_name='units_created_by', to=settings.AUTH_USER_MODEL)),
+                ('last_modifier', models.ForeignKey(related_name='units_last_modified_by', to=settings.AUTH_USER_MODEL)),
+                ('phenotype_variables', models.ManyToManyField(related_name='units_as_phenotype_trait', to='trait_browser.SourceTrait')),
+            ],
+            options={
+                'verbose_name': 'harmonization unit recipe',
+            },
+        ),
+        migrations.AddField(
+            model_name='harmonizationrecipe',
+            name='units',
+            field=models.ManyToManyField(verbose_name='harmonization units', to='recipes.UnitRecipe'),
+        ),
+    ]
