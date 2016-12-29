0b4cf68f7eae46b9c8bba1fdf3dde88f6d55ce36
Leslie Emery <emeryl@uw.edu>
2016-12-29 11:34:50 -0800

    Fill out docstrings in recipes/views.py

--- recipes/views.py
+++ recipes/views.py
@@ -18,65 +18,86 @@ formattable_valid_message = u'{} {} {}d!'
 
 
 class CreateUnitRecipe(LoginRequiredMixin, UserFormKwargsMixin, FormMessagesMixin, CreateView):
-    """ """
+    """Create form view class for UnitRecipe creation, used with UnitRecipeModelForm."""
     model = UnitRecipe
     form_class = UnitRecipeForm
     template_name = 'recipes/recipe_form.html'
     form_invalid_message = unit_invalid_message
 
     def form_valid(self, form):
-        # Add current user as the creator and last_modifier.
+        """Custom processing for valid forms.
+        
+        Sets current user as the UnitRecipe's creator and last_modifier.
+        """
         form.instance.creator = self.request.user
         form.instance.last_modifier = self.request.user
         return super(CreateUnitRecipe, self).form_valid(form)
     
     def get_form_valid_message(self):
+        """Add a session message about creation success."""
         return formattable_valid_message.format(UnitRecipe._meta.verbose_name, self.object.name, 'create')
 
 
 class CreateHarmonizationRecipe(LoginRequiredMixin, UserFormKwargsMixin, FormMessagesMixin, CreateView):
-    """ """
+    """Create form view class for HarmonizationRecipe creation, used with HarmonizationRecipeModelForm."""
     model = HarmonizationRecipe
     form_class = HarmonizationRecipeForm
     template_name = 'recipes/recipe_form.html'
     form_invalid_message = harmonization_invalid_message
 
     def form_valid(self, form):
+        """Custom processing for valid forms.
+        
+        Sets current user as the HarmonizationRecipe's creator and last_modifier.
+        """
         form.instance.creator = self.request.user
         form.instance.last_modifier = self.request.user
         return super(CreateHarmonizationRecipe, self).form_valid(form)
 
     def get_form_valid_message(self):
+        """Add a session message about creation success."""
         return formattable_valid_message.format(HarmonizationRecipe._meta.verbose_name.title(), self.object.name, 'create')
 
 
 class UpdateUnitRecipe(LoginRequiredMixin, UserFormKwargsMixin, FormMessagesMixin, UpdateView):
-    """ """
+    """Update form view class for UnitRecipe editing, used with UnitRecipeModelForm."""
     model = UnitRecipe
     form_class = UnitRecipeForm
     template_name = 'recipes/recipe_form.html'
     form_invalid_message = unit_invalid_message
 
     def form_valid(self, form):
+        """Custom processing for valid forms.
+        
+        Sets current user as the UnitRecipe's last_modifier.
+        """
         form.instance.last_modifier = self.request.user
+        # TODO: Add code here to make a new UnitRecipe instance, with a higher version number.
         return super(UpdateUnitRecipe, self).form_valid(form)
 
     def get_form_valid_message(self):
+        """Add a session message about editing success."""
         return formattable_valid_message.format(UnitRecipe._meta.verbose_name.title(), self.object.name, 'save')
 
 
 class UpdateHarmonizationRecipe(LoginRequiredMixin, UserFormKwargsMixin, FormMessagesMixin, UpdateView):
-    """ """
+    """Update form view class for HarmonizationRecipe editing, used with HarmonizationRecipeModelForm."""
     model = HarmonizationRecipe
     form_class = HarmonizationRecipeForm
     template_name = 'recipes/recipe_form.html'
     form_invalid_message = harmonization_invalid_message
 
     def form_valid(self, form):
+        """Custom processing for valid forms.
+        
+        Sets current user as the HarmonizationRecipe's last_modifier.
+        """
         form.instance.last_modifier = self.request.user
+        # TODO: Add code here to make a new HarmonizationRecipe instance, with a higher version number.
         return super(UpdateHarmonizationRecipe, self).form_valid(form)
 
     def get_form_valid_message(self):
+        """Add a session message about editing success."""
         return formattable_valid_message.format(HarmonizationRecipe._meta.verbose_name.title(), self.object.name, 'save')
 
 
@@ -87,7 +108,7 @@ class UnitRecipeIDAutocomplete(LoginRequiredMixin, autocomplete.Select2QuerySetV
     """    
     
     def get_queryset(self):
-        """Return a queryset of UnitRecipes whose pk starts with the """
+        """Return a queryset of UnitRecipes whose pk starts with the string from the GET request."""
         retrieved = UnitRecipe.objects.all()
         if self.q:
             retrieved = retrieved.filter(id__regex=r'^{}'.format(self.q))
