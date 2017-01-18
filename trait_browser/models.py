51ea0d62ded3272bb70311efc31244dd4d4fb395
Leslie Emery <emeryl@uw.edu>
2017-01-17 17:31:31 -0800

    Move TimeStampedModel from trait_browser.models to core.models
    Moving this abstract base class model to core.models will allow reuse
    of the code in the recipes app later.

--- trait_browser/models.py
+++ trait_browser/models.py
@@ -8,18 +8,7 @@
 
 from django.db import models
 
-class TimeStampedModel(models.Model):
-    """
-    An abstract base class model that provides selfupdating
-    ``created`` and ``modified`` fields.
-    """
-
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
-
+from core.models import TimeStampedModel
 
 # Study models.
 # ------------------------------------------------------------------------------
