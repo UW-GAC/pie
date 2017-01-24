2dca50d4e38cb48c16aeac3d683e0cb5ff1faccd
Jon Lin <jlin262@uw.edu>
2017-01-24 10:22:40 -0800

    Adds created/modified fields to UserData table

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,20 @@
 from django.conf import settings
 from django.db import models
 
-class UserData(models.Model):
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides selfupdating
+    ``created`` and ``modified`` fields.
+    """
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UserData(TimeStampedModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     saved_search_ids = models.CommaSeparatedIntegerField(max_length=255)
