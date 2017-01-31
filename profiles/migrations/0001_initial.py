20d5d508fd151065107068020f3a318786365b79
Jon Lin <jlin262@uw.edu>
2017-01-31 11:47:35 -0800

    Added search_type to distinguish Harmonized vs. Source

--- profiles/migrations/0001_initial.py
+++ profiles/migrations/0001_initial.py
@@ -8,19 +8,20 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('trait_browser', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Search',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('modified', models.DateTimeField(auto_now=True)),
-                ('param_text', models.CharField(max_length=100, db_index=True)),
+                ('param_text', models.CharField(db_index=True, max_length=100)),
                 ('search_count', models.IntegerField(default=1)),
+                ('search_type', models.CharField(max_length=25)),
                 ('param_studies', models.ManyToManyField(to='trait_browser.Study')),
             ],
             options={
@@ -30,7 +31,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='UserData',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('modified', models.DateTimeField(auto_now=True)),
                 ('saved_searches', models.ManyToManyField(to='profiles.Search')),
