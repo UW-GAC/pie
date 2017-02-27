94e725c76dbf27b8abf56d6feb21b41d60531d2e
Leslie Emery <emeryl@uw.edu>
2017-02-27 14:33:50 -0800

    Change variable name in _get_source_db() to source_db
    Change cnx to source_db, which is a more self-explanatory variable
    name.

--- trait_browser/management/commands/import_db.py
+++ trait_browser/management/commands/import_db.py
@@ -57,11 +57,10 @@ class Command(BaseCommand):
             raise ValueError('Requested full permissions for {} source database. Not allowed!!!')
         # Default is to connect as readonly; only test functions connect as full user.
         cnf_group = ['client', 'mysql_topmed_pheno_{}_{}'.format(permissions, which_db)]
-        cnx = mysql.connector.connect(option_files=cnf_path, option_groups=cnf_group, charset='latin1', use_unicode=False, time_zone='+00:00')
-        logger.debug('Connected to source db {}'.format(cnx))
+        source_db = mysql.connector.connect(option_files=cnf_path, option_groups=cnf_group, charset='latin1', use_unicode=False, time_zone='+00:00')
+        logger.debug('Connected to source db {}'.format(source_db))
         # TODO add a try/except block here in case the db connection fails.
-        return cnx
-
+        return source_db
 
     # Helper methods for data munging.
     def _fix_bytearray(self, row_dict):
