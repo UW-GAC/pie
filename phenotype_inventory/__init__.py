3a7eed46934bd653f5cfd982de9ccc5ef74086dd
Leslie Emery <emeryl@uw.edu>
2017-04-07 17:11:55 -0700

    Move versioning script into a management command in core
    Had trouble getting the versioning script to work on the staging server
    because I was doing a weird thing tying to make it a standalone python
    script instead of part of the django app. So make it a management command
    in the core app instead.

--- phenotype_inventory/__init__.py
+++ phenotype_inventory/__init__.py
@@ -1,7 +1,9 @@
 """
 """
 
-from .versioning import get_version_string
+from core.management.commands.increment_version import Command
 
 
-__version__ = get_version_string()
+cmd = Command()
+cmd.get_version_from_json()
+__version__ = cmd.get_version_string()
