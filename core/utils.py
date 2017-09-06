8eb82b83501b1201f7f70888d363a728bf911d34
Leslie Emery <emeryl@uw.edu>
2017-09-06 13:44:42 -0700

    flake8 fixes in all files from core app

--- core/utils.py
+++ core/utils.py
@@ -1,6 +1,6 @@
-"""
-Utility functions, to be used across the entire project. After suggestion from
-Two Scoops of Django 1.8.
+"""Utility functions, to be used across the entire project.
+
+Following a suggestion from Two Scoops of Django 1.8.
 """
 
 import exrex
@@ -10,59 +10,60 @@ from django.contrib.auth.models import Group
 from django.core.urlresolvers import reverse, RegexURLResolver, RegexURLPattern
 from django.test import TestCase, Client
 
-from .factories import *
+from . import factories
 
 User = get_user_model()
 
 
 def collect_all_urls(urlpatterns):
-    """Returns sample urls for views in app"""
+    """Returns sample urls for views in app."""
     url_list = []
     for root_pattern in urlpatterns:
         # first pattern is iterable, hence 1 element list
         find_all_urls([root_pattern], [], url_list)
     return url_list
 
+
 def find_all_urls(root_patterns, parents, url_list):
-    """Produce a url based on a pattern"""
+    """Produce a url based on a pattern."""
     for url in root_patterns:
         regex_string = url.regex.pattern
         if isinstance(url, RegexURLResolver):
             # print('Parent: ',regex_string)
             parents.append(next(exrex.generate(regex_string, limit=1)))
-            find_all_urls(url.url_patterns, parents, url_list) # call this function recursively
+            find_all_urls(url.url_patterns, parents, url_list)  # Call this function recursively.
         elif isinstance(url, RegexURLPattern):
             url_list.append(''.join(parents) + next(exrex.generate(regex_string, limit=1)))
 
 
 class UserLoginTestCase(TestCase):
     """TestCase that creates a user and logs in as that user.
-    
+
     Creates a user and logs them into the site, allowing access to the @login_required
     (or LoginRequiredMixin) views. Mostly used in test_views.py scripts.
     """
-    
+
     def setUp(self):
         super(UserLoginTestCase, self).setUp()
 
         self.client = Client()
-        self.user_password = USER_FACTORY_PASSWORD
-        self.user = UserFactory.create()
+        self.user_password = factories.USER_FACTORY_PASSWORD
+        self.user = factories.UserFactory.create()
         self.client.login(username=self.user.email, password=self.user_password)
 
 
 class DCCAnalystLoginTestCase(TestCase):
     """TestCase that creates a dcc_analyst user and logs in as that user.
-    
+
     Creates a user, adds them to dcc_analyst group, and logs them into the site.
     """
-    
+
     def setUp(self):
         super(DCCAnalystLoginTestCase, self).setUp()
 
         self.client = Client()
-        self.user_password = USER_FACTORY_PASSWORD
-        self.user = UserFactory.create()
+        self.user_password = factories.USER_FACTORY_PASSWORD
+        self.user = factories.UserFactory.create()
         dcc_analysts = Group.objects.get(name='dcc_analysts')
         self.user.groups.add(dcc_analysts)
         self.user.is_staff = True
@@ -73,16 +74,16 @@ class DCCAnalystLoginTestCase(TestCase):
 
 class DCCDeveloperLoginTestCase(TestCase):
     """TestCase that creates a dcc_developer user and logs in as that user.
-    
+
     Creates a user, adds them to dcc_developer group, and logs them into the site.
     """
-    
+
     def setUp(self):
         super(DCCDeveloperLoginTestCase, self).setUp()
 
         self.client = Client()
-        self.user_password = USER_FACTORY_PASSWORD
-        self.user = UserFactory.create()
+        self.user_password = factories.USER_FACTORY_PASSWORD
+        self.user = factories.UserFactory.create()
         dcc_developers = Group.objects.get(name='dcc_developers')
         self.user.groups.add(dcc_developers)
         self.user.is_staff = True
@@ -93,16 +94,16 @@ class DCCDeveloperLoginTestCase(TestCase):
 
 class RecipeSubmitterLoginTestCase(TestCase):
     """TestCase that creates a recipe_submitter user and logs in as that user.
-    
+
     Creates a user, adds them to recipe_submitter group, and logs them into the site.
     """
-    
+
     def setUp(self):
         super(RecipeSubmitterLoginTestCase, self).setUp()
 
         self.client = Client()
-        self.user_password = USER_FACTORY_PASSWORD
-        self.user = UserFactory.create()
+        self.user_password = factories.USER_FACTORY_PASSWORD
+        self.user = factories.UserFactory.create()
         recipe_submitters = Group.objects.get(name='recipe_submitters')
         self.user.groups.add(recipe_submitters)
         self.user.refresh_from_db()
@@ -111,21 +112,21 @@ class RecipeSubmitterLoginTestCase(TestCase):
 
 class SuperuserLoginTestCase(TestCase):
     """TestCase to use for accessing views that require Superuser login.
-    
+
     Creates a superuser and logs them into the site.
     """
-    
+
     def setUp(self):
         super(SuperuserLoginTestCase, self).setUp()
 
         self.client = Client()
-        self.user_password = USER_FACTORY_PASSWORD
-        self.user = SuperUserFactory.create()
-        self.client.login(username=self.user.email, password=self.user_password)        
+        self.user_password = factories.USER_FACTORY_PASSWORD
+        self.user = factories.SuperUserFactory.create()
+        self.client.login(username=self.user.email, password=self.user_password)
 
 
 class LoginRequiredTestCase(TestCase):
-    """Tests all views in this app that they are using login_required"""
+    """Tests all views in this app that they are using login_required."""
 
     def assert_redirect_all_urls(self, urlpatterns, pattern_root):
         """Use this in a subclass to ensure all urls from urlpatterns redirect to login."""
