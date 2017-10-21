91f3acf4c46ac8f028eeef186f0889cf6a50e413
Leslie Emery <emeryl@uw.edu>
2017-10-20 17:23:41 -0700

    Add tags models to the the admin site, and update Selenium admin site tests
    Set up ModelAdmin classes for Tag and TaggedTrait models. Register those models
    with the admin site. Went to add these to Selenium testing of the admin site and
    found that no tests had been added for newer trait_browser or recipes models. So
    split up the tests in AdminTestCase into separate tests for each of the apps.

---
+++ tags/admin.py
@@ -0,0 +1,40 @@
+"""Customization of the admin interface for the tags app."""
+
+from django.contrib import admin
+
+from . import models
+
+
+class TagAdmin(admin.ModelAdmin):
+    """Admin class for Tag objects."""
+
+    list_display = ('lower_title', 'creator', 'created', 'modified', )
+    list_filter = ('creator', )
+    search_fields = ('lower_title', 'description', )
+
+    def save_model(self, request, obj, form, change):
+        """Save current user as the Tag creator."""
+        if obj.pk is None:
+            obj.creator = request.user
+        obj.last_modifier = request.user
+        obj.save()
+
+
+class TaggedTraitAdmin(admin.ModelAdmin):
+    """Admin class for TaggedTrait objects."""
+
+    list_display = ('tag', 'trait', 'recommended', 'creator', 'created', 'modified', )
+    list_filter = ('tag', 'trait', 'creator', 'recommended', )
+    search_fields = ('tag', 'trait', )
+
+    def save_model(self, request, obj, form, change):
+        """Save current user as the TaggedTrait creator."""
+        if obj.pk is None:
+            obj.creator = request.user
+        obj.last_modifier = request.user
+        obj.save()
+
+
+# Register models for showing them in the admin interface.
+admin.site.register(models.Tag, TagAdmin)
+admin.site.register(models.TaggedTrait, TaggedTraitAdmin)
