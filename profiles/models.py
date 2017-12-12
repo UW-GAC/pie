c49e29fdba3d03f5a93699e34355b5056fa22c8f
Leslie Emery <emeryl@uw.edu>
2017-12-11 17:18:13 -0800

    Automatically create a profile for any new user
    Add receiver signals to profile.models to automatically create and save a Profile
    object for each user at appropriate times. Test the creation of the profile upon
    user creation in core.test_factories.

--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,9 @@
 from django.conf import settings
-from django.db import models
-from django.utils.http import urlencode
 from django.core.urlresolvers import reverse
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.http import urlencode
 
 from core.models import TimeStampedModel
 
@@ -48,6 +50,19 @@ class Profile(TimeStampedModel):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     saved_searches = models.ManyToManyField(Search, through="SavedSearchMeta")
     taggable_studies = models.ManyToManyField('trait_browser.Study')
+
+
+# Taken from post about how to extend the Django user model.
+# https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 class SavedSearchMeta(TimeStampedModel):
