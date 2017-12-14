3c13e642bb283bc27b91b9dc99e5ef307f2932a2
Leslie Emery <emeryl@uw.edu>
2017-12-13 16:21:32 -0800

    Clean up data migrations for groups and permissions
    The initial core migration, which loads a data fixture for default groups, didn't
    work properly because it linked the permissions for each group to the primary key
    of the builtin ContentType model. These pks can be different depending on how the
    migrations are run. For example, the production db has different pks for the content
    types than my development db does. So, rebuild the auth_group.json data fixture using
    the --natural-foreign option.
    
    This whole detour led me to do more reading on groups and permissions in Django, and
    I realized that I hadn't really gotten groups and permissions set up properly from
    the beginning. So I modified the core app's 0003_add_phenotype_taggers_auth_group
    migration to add permissions at the same time as the phenotype_taggers group is
    created and also wrote a new migration, 0004_update_group_permissions, which sets
    the permissions to what they should actually be. Since migration 3 hadn't been applied
    on the staging or production servers, I was okay with modifying an existing migration
    file, which I wouldn't otherwise do.

--- core/migrations/0002_initial_groups.py
+++ core/migrations/0002_initial_groups.py
@@ -4,8 +4,9 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 
 # Created the .json fixture file like so:
-# ./manage.py dumpdata --indent=2 auth.Group > core/fixtures/auth_group.json
-
+# ./manage.py dumpdata --indent=2 --natural-foreign auth.Group > core/fixtures/auth_group.json
+# 12/12/2017, 3:28:34 PM regenerated the fixutre with the natural keys options, based on permissions that are
+# in production website.
 
 def load_groups_from_fixture(apps, schema_editor):
     from django.core.management import call_command
--- core/migrations/0003_add_phenotype_taggers_auth_group.py
+++ core/migrations/0003_add_phenotype_taggers_auth_group.py
@@ -6,15 +6,34 @@ from django.conf import settings
 
 
 def add_phenotype_taggers_group(apps, schema_editor):
-    """Create the 'phenotype_taggers' group."""
+    """Create the 'phenotype_taggers' group and give tagging permissions."""
     Group = apps.get_model('auth', 'Group')
-    Group.objects.create(name='phenotype_taggers')
+    Permission = apps.get_model('auth', 'Permission')
+    # Get or create the phenotype_taggers group.
+    tagger_group = Group.objects.get_or_create(name='phenotype_taggers')[0]
+    analysts_group = Group.objects.get(name='dcc_analysts')
+    developers_group = Group.objects.get(name='dcc_developers')
+    # Add all permissions for the tags models to the group, and to DCC analysts.
+    tag_permissions = Permission.objects.filter(content_type__app_label='tags', content_type__model='tag')
+    analysts_group.permissions.add(*tag_permissions)
+    developers_group.permissions.add(*tag_permissions)
+    taggedtrait_permissions = Permission.objects.filter(
+        content_type__app_label='tags', content_type__model='taggedtrait')
+    tagger_group.permissions.add(*taggedtrait_permissions)
 
 
 def remove_phenotype_taggers_group(apps, schema_editor):
-    """Remove the 'phenotype_taggers' group."""
+    """Remove the 'phenotype_taggers' group and revoke tagging permissions."""
     Group = apps.get_model('auth', 'Group')
+    Permission = apps.get_model('auth', 'Permission')
+    # Delete the phenotype_taggers group.
     Group.objects.get(name='phenotype_taggers').delete()
+    # Remove permissions to the tags models from the DCC groups.
+    analysts_group = Group.objects.get(name='dcc_analysts')
+    developers_group = Group.objects.get(name='dcc_developers')
+    tag_permissions = Permission.objects.filter(content_type__app_label='tags')
+    analysts_group.permissions.remove(*tag_permissions)
+    developers_group.permissions.remove(*tag_permissions)
 
 
 class Migration(migrations.Migration):
