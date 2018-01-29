73d32df91b90d9fd7ea64adbd4d4598be8061749
Leslie Emery <emeryl@uw.edu>
2018-01-29 13:43:17 -0800

    flake8 fix in tags.tables

--- tags/tables.py
+++ tags/tables.py
@@ -54,7 +54,8 @@ class StudyTaggedTraitTable(tables.Table):
     """Table for displaying studies with tagged traits and totals."""
 
     i_study_name = tables.LinkColumn(
-        'trait_browser:source:studies:detail:tagged', args=[tables.utils.A('pk')], verbose_name='Study name', orderable=False)
+        'trait_browser:source:studies:detail:tagged', args=[tables.utils.A('pk')], verbose_name='Study name',
+        orderable=False)
     number_tags = tables.Column(
         accessor='get_tag_count', verbose_name='Number of tags', orderable=False)
     number_traits = tables.Column(
