ce3e3197f22d7ff103c387917d670c684727b977
Leslie Emery <emeryl@uw.edu>
2018-01-30 17:00:59 -0800

    Add tests for new table classes

--- trait_browser/test_tables.py
+++ trait_browser/test_tables.py
@@ -100,11 +100,71 @@ class SourceDatasetTableTest(TestCase):
         self.assertEqual(row.get_cell('trait_count'), '{:,}'.format(n_traits))
 
 
+class SourceDatasetTableFullTest(TestCase):
+
+    model = models.SourceDataset
+    model_factory = factories.SourceDatasetFactory
+    table_class = tables.SourceDatasetTableFull
+
+    def test_row_count(self):
+        """Table has expected number of rows."""
+        things = self.model_factory.create_batch(20)
+        table = self.table_class(things)
+        self.assertEqual(self.model.objects.count(), len(table.rows))
+
+    def test_trait_count_zero(self):
+        """Trait count is correct when there are zero traits for a dataset."""
+        things = self.model_factory.create_batch(20)
+        table = self.table_class(things)
+        row = table.rows[0]
+        n_traits = 0
+        # Remake the table, to update trait counts.
+        table = self.table_class(things)
+        row = table.rows[0]
+        self.assertEqual(row.get_cell('trait_count'), '{:,}'.format(n_traits))
+
+    def test_trait_count_12(self):
+        """Trait count is correct when there are 12 traits in the dataset."""
+        things = self.model_factory.create_batch(20, source_study_version__i_is_deprecated=False)
+        table = self.table_class(things)
+        row = table.rows[0]
+        n_traits = 12
+        factories.SourceTraitFactory.create_batch(
+            n_traits, source_dataset=row.record)
+        # Remake the table, to update trait counts.
+        table = self.table_class(things)
+        row = table.rows[0]
+        self.assertEqual(row.get_cell('trait_count'), '{:,}'.format(n_traits))
+
+
 class SourceTraitTableTest(TestCase):
 
     model = models.SourceTrait
     model_factory = factories.SourceTraitFactory
     table_class = tables.SourceTraitTable
+
+    def test_row_count(self):
+        """Table has expected number of rows."""
+        things = self.model_factory.create_batch(20)
+        table = self.table_class(things)
+        self.assertEqual(self.model.objects.count(), len(table.rows))
+
+    def test_trait_name(self):
+        """Trait name column value is as expected."""
+        things = self.model_factory.create_batch(20)
+        table = self.table_class(things)
+        row = table.rows[0]
+        self.assertIn(
+            row.record.i_trait_name,
+            row.get_cell_value('i_trait_name')
+        )
+
+
+class SourceTraitTableFullTest(TestCase):
+
+    model = models.SourceTrait
+    model_factory = factories.SourceTraitFactory
+    table_class = tables.SourceTraitTableFull
 
     def test_row_count(self):
         """Table has expected number of rows."""
