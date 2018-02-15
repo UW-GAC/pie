d3ddf026b1d9b5d88fffcc245deafacab6443fb3
Adrienne Stilp <amstilp@uw.edu>
2018-02-14 16:53:08 -0800

    Add docstrings to custom queryset methods

--- trait_browser/querysets.py
+++ trait_browser/querysets.py
@@ -3,14 +3,17 @@ from django.db import models
 
 class SourceDatasetQuerySet(models.query.QuerySet):
     def current(self):
+        """Filter to non-deprecated source datasets."""
         return self.filter(source_study_version__i_is_deprecated=False)
 
 
 class SourceTraitQuerySet(models.query.QuerySet):
     def current(self):
+        """Filter to non-deprecated source traits."""
         return self.filter(source_dataset__source_study_version__i_is_deprecated=False)
 
 
 class HarmonizedTraitQuerySet(models.query.QuerySet):
     def current(self):
+        """Filter to non-deprecated harmonized traits."""
         return self.filter(harmonized_trait_set_version__i_is_deprecated=False)
