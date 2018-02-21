34ac8c0ccfe4d21d3f660f18ffe4d7da31bd510f
Leslie Emery <emeryl@uw.edu>
2018-02-21 15:55:07 -0800

    Add study tagged traits table to profile
    Add a tab with all of the tagged traits (no matter who they were tagged
    by) for the user's taggable studies to the profile page.

--- profiles/views.py
+++ profiles/views.py
@@ -7,7 +7,7 @@ from braces.views import LoginRequiredMixin
 import recipes.models
 import recipes.tables
 from tags.models import TaggedTrait
-from tags.tables import UserTaggedTraitTable
+from tags.tables import TaggedTraitTableWithDelete, UserTaggedTraitTable
 from trait_browser.models import Study
 
 
@@ -30,6 +30,13 @@ class Profile(LoginRequiredMixin, TemplateView):
         for pk in study_pks:
             studies.append(Study.objects.get(pk=pk))
             taggedtrait_tables_by_study.append(UserTaggedTraitTable(
-                TaggedTrait.objects.filter(trait__source_dataset__source_study_version__study__pk=pk)))
+                user_tagged_traits.filter(trait__source_dataset__source_study_version__study__pk=pk)))
         context['study_table_tuple'] = zip(studies, taggedtrait_tables_by_study)
+        all_taggedtrait_tables_by_study = []
+        taggable_studies = self.request.user.profile.taggable_studies.all()
+        if len(taggable_studies) > 0:
+            for study in taggable_studies:
+                all_taggedtrait_tables_by_study.append(TaggedTraitTableWithDelete(
+                    TaggedTrait.objects.filter(trait__source_dataset__source_study_version__study=study)))
+            context['all_taggedtraits_study_table_tuple'] = zip(taggable_studies, all_taggedtrait_tables_by_study)
         return context
