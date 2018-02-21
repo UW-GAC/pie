1107cb35a9257b94eb277d8d006b6e8471c1204e
Leslie Emery <emeryl@uw.edu>
2018-02-20 16:49:54 -0800

    Make Profile into a CBV
    Convert the old profiles.views.profile FBV into the new
    profiles.views.Profile CBV. Also change where the page title is set - do
    it statically in the template, not in a context variable.

--- profiles/views.py
+++ profiles/views.py
@@ -1,24 +1,22 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+"""Views for the profiles app."""
+
+from django.views.generic import TemplateView
+
+from braces.views import LoginRequiredMixin
 
 import recipes.models
 import recipes.tables
-from . import models
-from . import tables
 
 
-@login_required
-def profile(request):
-    page_title = 'My profile'
-    user_unit_recipes = recipes.models.UnitRecipe.objects.filter(creator=request.user).order_by('-modified')
-    unit_recipe_table = recipes.tables.UnitRecipeTable(user_unit_recipes)
-    user_harmonization_recipes = recipes.models.HarmonizationRecipe.objects.filter(
-        creator=request.user).order_by('-modified')
-    harmonization_recipe_table = recipes.tables.HarmonizationRecipeTable(user_harmonization_recipes)
+class Profile(LoginRequiredMixin, TemplateView):
 
-    return render(
-        request,
-        'profiles/profile.html',
-        {'page_title': page_title,
-         'unit_recipe_table': unit_recipe_table, 'harmonization_recipe_table': harmonization_recipe_table}
-    )
+    template_name = 'profiles/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Profile, self).get_context_data(**kwargs)
+        user_unit_recipes = recipes.models.UnitRecipe.objects.filter(creator=self.request.user).order_by('-modified')
+        context['unit_recipe_table'] = recipes.tables.UnitRecipeTable(user_unit_recipes)
+        user_harmonization_recipes = recipes.models.HarmonizationRecipe.objects.filter(
+            creator=self.request.user).order_by('-modified')
+        context['harmonization_recipe_table'] = recipes.tables.HarmonizationRecipeTable(user_harmonization_recipes)
+        return context
