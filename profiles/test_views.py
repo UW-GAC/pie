8c826e61d73e6efebe943bab6842f99501eac750
Leslie Emery <emeryl@uw.edu>
2018-02-22 17:29:03 -0800

    Revise profile page and add tests
    Add tests of the profile view for 1) regular users, 2) DCC analysts,
    3) recipe submitters, and 4) phenotype taggers. In the profile view,
    add variables controlling whether to show each of the elements (unit
    recipes, harmonization recipes, my tagged traits, and my study's tagged
    traits). Modify the view a bit to properly display the page when a DCC
    analyst or a phenotype tagger does not have any traits that they have
    tagged.

--- profiles/test_views.py
+++ profiles/test_views.py
@@ -3,30 +3,246 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-from . import models
-from . import factories
+from core.utils import (DCCAnalystLoginTestCase, LoginRequiredTestCase, RecipeSubmitterLoginTestCase,
+                        PhenotypeTaggerLoginTestCase, UserLoginTestCase)
+from recipes.factories import HarmonizationRecipeFactory, UnitRecipeFactory
+from tags.factories import TaggedTraitFactory
 from trait_browser.factories import StudyFactory
 
-from core.utils import LoginRequiredTestCase, UserLoginTestCase
 
+class ProfileTest(UserLoginTestCase):
 
-class ProfileViewTestCase(UserLoginTestCase):
+    def setUp(self):
+        super(ProfileTest, self).setUp()
 
-    def test_working(self):
-        url = reverse('profiles:profile')
-        response = self.client.get(url)
-        # Does the URL work?
+    def get_url(self, *args):
+        return reverse('profiles:profile')
+
+    def test_view_success_code(self):
+        """View returns successful response code."""
+        response = self.client.get(self.get_url())
         self.assertEqual(response.status_code, 200)
 
+    def test_context_data(self):
+        """View has appropriate data in the context."""
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertFalse(context['show_tabs'])
+        self.assertFalse(context['show_recipes'])
+        self.assertFalse(context['show_my_tagged'])
+        self.assertFalse(context['show_study_tagged'])
+        self.assertNotIn('unit_recipe_table', context)
+        self.assertNotIn('harmonization_recipe_table', context)
+        self.assertNotIn('user_tagged_tables', context)
+        self.assertNotIn('taggable_study_tagged_tables', context)
+        self.assertEqual(context['user'], self.user)
 
-class ProfileViewWithoutLoginTestCase(TestCase):
+    def test_no_tagged_phenotypes(self):
+        """Regular user does not see My Tagged Phenotypes."""
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertNotContains(response, 'My Tagged Phenotypes')
+        self.assertNotContains(response, 'Tagged Phenotypes in My Studies')
 
-    def test_redirects(self):
-        url = reverse('profiles:profile')
-        response = self.client.get(url)
-        # Redirected?
-        expected_url = '{url}?next={redirect}'.format(redirect=reverse('profiles:profile'), url=reverse('login'))
-        self.assertRedirects(response, expected_url)
+    def test_no_recipes(self):
+        """Regular user does not see any recipes."""
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertFalse(context['show_recipes'])
+        self.assertNotContains(response, 'My Unit Recipes')
+        self.assertNotContains(response, 'My Harmonization Recipes')
+
+
+class DCCAnalystLoginTestCaseProfileTest(DCCAnalystLoginTestCase):
+
+    def setUp(self):
+        super(DCCAnalystLoginTestCaseProfileTest, self).setUp()
+
+    def get_url(self, *args):
+        return reverse('profiles:profile')
+
+    def test_view_success_code(self):
+        """View returns successful response code."""
+        response = self.client.get(self.get_url())
+        self.assertEqual(response.status_code, 200)
+
+    def test_context_data(self):
+        """View has appropriate data in the context."""
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertTrue(context['show_tabs'])
+        self.assertTrue(context['show_recipes'])
+        self.assertTrue(context['show_my_tagged'])
+        self.assertFalse(context['show_study_tagged'])
+        self.assertIn('unit_recipe_table', context)
+        self.assertEqual(len(context['unit_recipe_table'].rows), 0)
+        self.assertIn('harmonization_recipe_table', context)
+        self.assertEqual(len(context['harmonization_recipe_table'].rows), 0)
+        self.assertIn('user_tagged_tables', context)
+        self.assertEqual(len(context['user_tagged_tables']), 0)
+        self.assertNotIn('taggable_study_tagged_tables', context)
+
+    def test_has_tagged_phenotypes(self):
+        """Staff user does see My Tagged Phenotypes, but not study tagged phenotypes."""
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertContains(response, 'My Tagged Phenotypes')
+        self.assertNotContains(response, 'Tagged Phenotypes in My Studies')
+
+    def test_has_recipes(self):
+        """Staff user does see any recipes."""
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertTrue(context['show_recipes'])
+        self.assertContains(response, 'My Unit Recipes')
+        self.assertContains(response, 'My Harmonization Recipes')
+
+    def test_has_correct_recipe_count(self):
+        """Tables of recipes have the correct number of rows for this user."""
+        unit_recipes = UnitRecipeFactory.create_batch(10, creator=self.user)
+        harmonization_recipes = HarmonizationRecipeFactory.create_batch(10, creator=self.user)
+        other_unit_recipe = UnitRecipeFactory.create()
+        other_harmonization_recipe = HarmonizationRecipeFactory.create()
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertNotIn(other_unit_recipe, context['unit_recipe_table'].data)
+        self.assertNotIn(other_harmonization_recipe, context['harmonization_recipe_table'].data)
+        self.assertEqual(len(context['unit_recipe_table'].rows), len(unit_recipes))
+        self.assertEqual(len(context['harmonization_recipe_table'].rows), len(harmonization_recipes))
+
+    def test_has_correct_taggedtrait_count(self):
+        """Table of tagged traits has the correct number of rows for this user."""
+        # There are no tables at all (an empty tab) when there are no tagged traits for this DCC analyst.
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertEqual(context['user_tagged_tables'], [])
+        # Count in one table is correct when the user has tagged studies.
+        study = StudyFactory.create()
+        tagged_traits = TaggedTraitFactory.create_batch(
+            10, creator=self.user,
+            trait__source_dataset__source_study_version__study=study)
+        other_tagged_trait = TaggedTraitFactory.create(trait__source_dataset__source_study_version__study=study)
+        response = self.client.get(self.get_url())
+        context = response.context
+        user_table = context['user_tagged_tables'][0][1]
+        self.assertNotIn(other_tagged_trait, user_table.data)
+        self.assertEqual(len(tagged_traits), len(user_table.rows))
+
+
+class RecipeSubmitterLoginTestCaseProfileTest(RecipeSubmitterLoginTestCase):
+
+    def setUp(self):
+        super(RecipeSubmitterLoginTestCaseProfileTest, self).setUp()
+
+    def get_url(self, *args):
+        return reverse('profiles:profile')
+
+    def test_view_success_code(self):
+        """View returns successful response code."""
+        response = self.client.get(self.get_url())
+        self.assertEqual(response.status_code, 200)
+
+    def test_context_data(self):
+        """View has appropriate data in the context."""
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertIn('show_tabs', context)
+        self.assertTrue(context['show_tabs'])
+        self.assertIn('show_recipes', context)
+        self.assertIn('unit_recipe_table', context)
+        self.assertIn('harmonization_recipe_table', context)
+        self.assertNotIn('user_tagged_tables', context)
+        self.assertNotIn('taggable_study_tagged_tables', context)
+
+    def test_no_tagged_phenotypes(self):
+        """Regular user does not see My Tagged Phenotypes."""
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertNotContains(response, 'My Tagged Phenotypes')
+        self.assertNotContains(response, 'Tagged Phenotypes in My Studies')
+
+    def test_no_recipes(self):
+        """Regular user does not see any recipes."""
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertTrue(context['show_recipes'])
+        self.assertContains(response, 'My Unit Recipes')
+        self.assertContains(response, 'My Harmonization Recipes')
+
+    def test_has_correct_recipe_count(self):
+        """Tables of recipes have the correct number of rows for this user."""
+        unit_recipes = UnitRecipeFactory.create_batch(10, creator=self.user)
+        harmonization_recipes = HarmonizationRecipeFactory.create_batch(10, creator=self.user)
+        other_unit_recipe = UnitRecipeFactory.create()
+        other_harmonization_recipe = HarmonizationRecipeFactory.create()
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertNotIn(other_unit_recipe, context['unit_recipe_table'].data)
+        self.assertNotIn(other_harmonization_recipe, context['harmonization_recipe_table'].data)
+        self.assertEqual(len(context['unit_recipe_table'].rows), len(unit_recipes))
+        self.assertEqual(len(context['harmonization_recipe_table'].rows), len(harmonization_recipes))
+
+
+class PhenotypeTaggerLoginTestCaseProfileTest(PhenotypeTaggerLoginTestCase):
+
+    def setUp(self):
+        super(PhenotypeTaggerLoginTestCaseProfileTest, self).setUp()
+
+    def get_url(self, *args):
+        return reverse('profiles:profile')
+
+    def test_view_success_code(self):
+        """View returns successful response code."""
+        response = self.client.get(self.get_url())
+        self.assertEqual(response.status_code, 200)
+
+    def test_context_data(self):
+        """View has appropriate data in the context."""
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertTrue(context['show_tabs'])
+        self.assertFalse(context['show_recipes'])
+        self.assertTrue(context['show_my_tagged'])
+        self.assertTrue(context['show_study_tagged'])
+        self.assertNotIn('unit_recipe_table', context)
+        self.assertNotIn('harmonization_recipe_table', context)
+        self.assertIn('user_tagged_tables', context)
+        self.assertIn('taggable_study_tagged_tables', context)
+
+    def test_no_tagged_phenotypes(self):
+        """Tagger user does see My Tagged Phenotypes."""
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertContains(response, 'My Tagged Phenotypes')
+        self.assertContains(response, 'Tagged Phenotypes in My Studies')
+
+    def test_no_recipes(self):
+        """Regular user does not see any recipes."""
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertFalse(context['show_recipes'])
+        self.assertNotContains(response, 'My Unit Recipes')
+        self.assertNotContains(response, 'My Harmonization Recipes')
+
+    def test_has_correct_taggedtrait_count(self):
+        """Table of tagged traits by user and by study have the correct number of rows."""
+        # There are no tables at all (an empty tab) when there are no tagged traits for this DCC analyst.
+        response = self.client.get(self.get_url())
+        context = response.context
+        self.assertEqual(context['user_tagged_tables'], [])
+        # Count in one table is correct when the user has tagged studies.
+        tagged_traits = TaggedTraitFactory.create_batch(
+            10, creator=self.user,
+            trait__source_dataset__source_study_version__study=self.study)
+        other_tagged_trait = TaggedTraitFactory.create(trait__source_dataset__source_study_version__study=self.study)
+        response = self.client.get(self.get_url())
+        context = response.context
+        user_table = context['user_tagged_tables'][0][1]
+        self.assertNotIn(other_tagged_trait, user_table.data)
+        self.assertEqual(len(tagged_traits), len(user_table.rows))
+        study_table = context['taggable_study_tagged_tables'][0][1]
+        self.assertIn(other_tagged_trait, study_table.data)
+        self.assertEqual(len(tagged_traits) + 1, len(study_table.rows))
 
 
 class ProfilesLoginRequiredTestCase(LoginRequiredTestCase):
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 """Views for the profiles app."""
 
+from django.contrib.auth.models import Group
 from django.views.generic import TemplateView
 
 from braces.views import LoginRequiredMixin
@@ -17,26 +18,52 @@ class Profile(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(Profile, self).get_context_data(**kwargs)
-        user_unit_recipes = recipes.models.UnitRecipe.objects.filter(creator=self.request.user).order_by('-modified')
-        context['unit_recipe_table'] = recipes.tables.UnitRecipeTable(user_unit_recipes)
-        user_harmonization_recipes = recipes.models.HarmonizationRecipe.objects.filter(
-            creator=self.request.user).order_by('-modified')
-        context['harmonization_recipe_table'] = recipes.tables.HarmonizationRecipeTable(user_harmonization_recipes)
-        user_tagged_traits = TaggedTrait.objects.filter(creator=self.request.user)
-        study_pks = set(user_tagged_traits.values_list(
-            'trait__source_dataset__source_study_version__study__pk', flat=True))
-        taggedtrait_tables_by_study = []
-        studies = []
-        for pk in study_pks:
-            studies.append(Study.objects.get(pk=pk))
-            taggedtrait_tables_by_study.append(UserTaggedTraitTable(
-                user_tagged_traits.filter(trait__source_dataset__source_study_version__study__pk=pk)))
-        context['study_table_tuple'] = zip(studies, taggedtrait_tables_by_study)
-        all_taggedtrait_tables_by_study = []
-        taggable_studies = self.request.user.profile.taggable_studies.all()
-        if len(taggable_studies) > 0:
-            for study in taggable_studies:
-                all_taggedtrait_tables_by_study.append(TaggedTraitTableWithDelete(
-                    TaggedTrait.objects.filter(trait__source_dataset__source_study_version__study=study)))
-            context['all_taggedtraits_study_table_tuple'] = zip(taggable_studies, all_taggedtrait_tables_by_study)
+        phenotype_tagger = Group.objects.get(name='phenotype_taggers')
+        recipe_submitter = Group.objects.get(name='recipe_submitters')
+        is_staff = self.request.user.is_staff
+        is_phenotype_tagger = phenotype_tagger in self.request.user.groups.all()
+        is_recipe_submitter = recipe_submitter in self.request.user.groups.all()
+        # Set up variables for controlling which components show up on the page.
+        context['show_tabs'] = False
+        context['show_recipes'] = False
+        context['show_my_tagged'] = False
+        context['show_study_tagged'] = False
+        # Show the tab panels only for staff members, phenotype_taggers, or recipe_submitters.
+        if is_phenotype_tagger or is_recipe_submitter or is_staff:
+            context['show_tabs'] = True
+        # Get recipes for recipe_submitters and staff.
+        if is_recipe_submitter or is_staff:
+            context['show_recipes'] = True
+            user_unit_recipes = recipes.models.UnitRecipe.objects.filter(
+                creator=self.request.user).order_by('-modified')
+            context['unit_recipe_table'] = recipes.tables.UnitRecipeTable(user_unit_recipes)
+            user_harmonization_recipes = recipes.models.HarmonizationRecipe.objects.filter(
+                creator=self.request.user).order_by('-modified')
+            context['harmonization_recipe_table'] = recipes.tables.HarmonizationRecipeTable(user_harmonization_recipes)
+        # Get tagged traits and their study names for phenotype_taggers and staff.
+        if is_phenotype_tagger or is_staff:
+            context['show_my_tagged'] = True
+            user_tagged_traits = TaggedTrait.objects.filter(creator=self.request.user)
+            context['user_tagged_traits'] = user_tagged_traits
+            user_tagged_study_pks = set(user_tagged_traits.values_list(
+                'trait__source_dataset__source_study_version__study__pk', flat=True))
+            context['user_tagged_study_pks'] = user_tagged_study_pks
+            user_tagged_tables = []
+            user_tagged_studies = []
+            for pk in user_tagged_study_pks:
+                user_tagged_studies.append(Study.objects.get(pk=pk))
+                user_tagged_tables.append(UserTaggedTraitTable(
+                    user_tagged_traits.filter(trait__source_dataset__source_study_version__study__pk=pk)))
+            # Force the zip() to evaluate so that tests work properly.
+            context['user_tagged_tables'] = list(zip(user_tagged_studies, user_tagged_tables))
+        if is_phenotype_tagger:
+            context['show_study_tagged'] = True
+            taggable_study_tagged_tables = []
+            taggable_studies = self.request.user.profile.taggable_studies.all()
+            if len(taggable_studies) > 0:
+                for study in taggable_studies:
+                    taggable_study_tagged_tables.append(TaggedTraitTableWithDelete(
+                        TaggedTrait.objects.filter(trait__source_dataset__source_study_version__study=study)))
+                # Force the zip() to evaluate so that tests work properly.
+                context['taggable_study_tagged_tables'] = list(zip(taggable_studies, taggable_study_tagged_tables))
         return context
