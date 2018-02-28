e0beebdf725ae62fff60625d3d3e3da7303efa73
Adrienne Stilp <amstilp@uw.edu>
2018-02-28 13:51:16 -0800

    Register harmonized traits with watson
    Register harmonied traits with the watson search. Include only the
    description for searching.

--- trait_browser/apps.py
+++ trait_browser/apps.py
@@ -14,10 +14,26 @@ class SourceTraitSearchAdapter(watson.SearchAdapter):
         return ''
 
 
+class HarmonizedTraitSearchAdapter(watson.SearchAdapter):
+
+    def get_title(self, obj):
+        return ''
+
+    def get_description(self, obj):
+        return obj.i_description
+
+    def get_content(self, obj):
+        return ''
+
+
 class TraitBrowserConfig(AppConfig):
 
     name = 'trait_browser'
 
     def ready(self):
+        # Register source traits.
         SourceTrait = self.get_model("SourceTrait")
         watson.register(SourceTrait, SourceTraitSearchAdapter, fields=('i_description',))
+        # Register harmonized traits.
+        HarmonizedTrait = self.get_model("HarmonizedTrait")
+        watson.register(HarmonizedTrait, HarmonizedTraitSearchAdapter, fields=('i_description',))
