5e05d7b9068ad85a72ea9821e87b2e8591967a92
Adrienne Stilp <amstilp@uw.edu>
2018-05-24 14:47:16 -0700

    Enable study field only after a tag is selected
    Add jQuery handlers to the custom javascript for the tagged trait
    tag/study select page to load the page with a disabled study field,
    and to enable it when the tag field is changed (selected). This
    prevents users from selecting a study before they select a tag.

--- static/js/taggedtrait_review_select_form.js
+++ static/js/taggedtrait_review_select_form.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
 
+  var tag_field = $("select[name$=tag]");
+  var study_field = $("select[name$=study]");
+
+  // Load the page with a disabled study field.
+  study_field.prop('disabled', true);
+
   // Reset the study field if the tag field changes.
-  $(':input[name$=tag]').on('change', function() {
+  tag_field.on('change', function() {
     // Get the field prefix, ie. if this comes from a formset form
     var prefix = $(this).getFormPrefix();
+    console.log(prefix)
 
     // Clear the autocomplete with the same prefix
     $(':input[name=' + prefix + 'study]').val(null).trigger('change');
+
+    // Remove the disabled attribute from the study field.
+    study_field.prop('disabled', false);
   });
 
 })
