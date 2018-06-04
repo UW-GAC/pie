f733878a08de24b84a3a11233f778cfc3164a45a
Leslie Emery <emeryl@uw.edu>
2018-06-04 16:19:51 -0700

    Remove outdated comment

--- tags/querysets.py
+++ tags/querysets.py
@@ -12,9 +12,5 @@ class TaggedTraitQuerySet(models.query.QuerySet):
 
     def need_followup(self):
         """Filter to TaggedTrait object that need study followup only."""
-        # We can't import from models.py because of models.py imports
-        # querysets.py. There might be a way to get the DCCReview model
-        # constants without importing models.py, but I haven't been able to
-        # figure out how.
         followup_code = self.model._meta.get_field('dcc_review').related_model.STATUS_FOLLOWUP
         return self.filter(dcc_review__status=followup_code)
