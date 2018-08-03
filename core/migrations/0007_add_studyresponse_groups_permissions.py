bc41b032356a19d56fb91b7e22eb8015191b71bf
Adrienne Stilp <amstilp@uw.edu>
2018-08-03 15:11:15 -0700

    Add permissions for the StudyResponse model
    Add a migration that grants DCC developers all permissions for the
    StudyResponse model.

---
+++ core/migrations/0007_add_studyresponse_groups_permissions.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.core.management.sql import emit_post_migrate_signal
+from django.db import migrations, models
+
+
+def set_permissions_for_studyresponse(apps, schema_editor):
+    """Add permissions for the tags.StudyResponse model."""
+    # In a migration from scratch, Django creates all of the models from all of the migrations, then runs
+    # post-migrate signals that create the matching contenttypes and permissions for those models. So to
+    # have the permissions available as needed, force the post-migrate signal to run now.
+    # https://code.djangoproject.com/ticket/23422#comment:20
+    db_alias = schema_editor.connection.alias
+    try:
+        emit_post_migrate_signal(2, False, db_alias)
+    except TypeError:
+        # Django < 1.9
+        try:
+            # Django 1.8
+            emit_post_migrate_signal(2, False, 'default', db_alias)
+        except TypeError:  # Django < 1.8
+            emit_post_migrate_signal([], 2, False, 'default', db_alias)
+    Group = apps.get_model('auth', 'Group')
+    Permission = apps.get_model('auth', 'Permission')
+    study_response_permissions = Permission.objects.filter(content_type__app_label='tags',
+                                                           content_type__model='StudyResponse')
+    # Give only developers all permissions on the Profile model.
+    developers = Group.objects.get(name='dcc_developers')
+    developers.permissions.add(*study_response_permissions)
+
+
+def delete_permissions_for_studyresponse(apps, schema_editor):
+    """Delete permissions for the tags.StudyResponse model."""
+    Group = apps.get_model('auth', 'Group')
+    Permission = apps.get_model('auth', 'Permission')
+    study_response_permissions = Permission.objects.filter(content_type__app_label='tags',
+                                                           content_type__model='StudyResponse')
+    # Revoke all permissions on the Profile model from developers.
+    developers = Group.objects.get(name='dcc_developers')
+    developers.permissions.remove(*study_response_permissions)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0006_add_dccreview_group_permissions'),
+        ('tags', '0004_studyresponse')
+    ]
+
+    operations = [
+        migrations.RunPython(set_permissions_for_studyresponse, reverse_code=delete_permissions_for_studyresponse),
+    ]
