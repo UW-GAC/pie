a99f54e39ed4d7117ffed76ba611a51d9450d873
Leslie Emery <emeryl@uw.edu>
2018-08-27 16:40:35 -0700

    Add ManyTaggedTraitsCreateByTagTestsMixin
    Add ManyTaggedTraitsCreateByTagTestsMixin class and have
    ManyTaggedTraitsCreateByTagPhenotypeTaggerTest and
    ManyTaggedTraitsCreateByTagDCCAnalystTest inherit from it.

--- tags/test_views.py
+++ tags/test_views.py
@@ -1382,13 +1382,7 @@ class ManyTaggedTraitsCreateDCCAnalystTest(ManyTaggedTraitsCreateTestsMixin, DCC
         self.assertEqual(response.status_code, 200)
 
 
-class ManyTaggedTraitsCreateByTagTest(PhenotypeTaggerLoginTestCase):
-
-    def setUp(self):
-        super(ManyTaggedTraitsCreateByTagTest, self).setUp()
-        self.tag = factories.TagFactory.create()
-        self.traits = SourceTraitFactory.create_batch(10, source_dataset__source_study_version__study=self.study)
-        self.user.refresh_from_db()
+class ManyTaggedTraitsCreateByTagTestsMixin(object):
 
     def get_url(self, *args):
         """Get the url for the view this class is supposed to test."""
@@ -1452,6 +1446,82 @@ class ManyTaggedTraitsCreateByTagTest(PhenotypeTaggerLoginTestCase):
         self.assertEqual(len(messages), 1)
         self.assertFalse('Oops!' in str(messages[0]))
 
+    def test_invalid_form_message(self):
+        """Posting invalid data results in a message about the invalidity."""
+        response = self.client.post(self.get_url(self.tag.pk), {'traits': '', })
+        messages = list(response.wsgi_request._messages)
+        self.assertEqual(len(messages), 1)
+        self.assertTrue('Oops!' in str(messages[0]))
+
+    def test_post_blank_trait(self):
+        """Posting bad data to the form doesn't tag the trait and shows a form error."""
+        response = self.client.post(self.get_url(self.tag.pk), {'traits': '', })
+        messages = list(response.wsgi_request._messages)
+        self.assertEqual(len(messages), 1)
+        self.assertTrue('Oops!' in str(messages[0]))
+        form = response.context['form']
+        self.assertTrue(form.has_error('traits'))
+        self.assertNotIn(self.tag, self.traits[0].tag_set.all())
+
+    def test_adds_user(self):
+        """When a trait is successfully tagged, it has the appropriate creator."""
+        response = self.client.post(self.get_url(self.tag.pk),
+                                    {'traits': [str(self.traits[0].pk)], })
+        new_object = models.TaggedTrait.objects.latest('pk')
+        self.assertEqual(self.user, new_object.creator)
+
+    def test_fails_when_one_trait_is_already_tagged(self):
+        """Tagging traits fails when a selected trait is already tagged with the tag."""
+        already_tagged = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[0])
+        response = self.client.post(self.get_url(self.tag.pk),
+                                    {'traits': [t.pk for t in self.traits[0:5]], })
+        self.assertEqual(response.status_code, 200)
+        messages = list(response.wsgi_request._messages)
+        self.assertEqual(len(messages), 1)
+        self.assertTrue('Oops!' in str(messages[0]))
+
+    def test_fails_when_two_traits_are_already_tagged(self):
+        """Tagging traits fails when two selected traits are already tagged with the tag."""
+        already_tagged = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[0])
+        already_tagged2 = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[1])
+        response = self.client.post(self.get_url(self.tag.pk),
+                                    {'traits': [t.pk for t in self.traits[0:5]], })
+        self.assertEqual(response.status_code, 200)
+        messages = list(response.wsgi_request._messages)
+        self.assertEqual(len(messages), 1)
+        self.assertTrue('Oops!' in str(messages[0]))
+
+    def test_fails_when_one_trait_is_already_tagged_but_archived(self):
+        """Tagging traits fails when a selected trait is already tagged with the tag but archived."""
+        already_tagged = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[0], archived=True)
+        response = self.client.post(self.get_url(self.tag.pk),
+                                    {'traits': [t.pk for t in self.traits[0:5]], })
+        self.assertEqual(response.status_code, 200)
+        messages = list(response.wsgi_request._messages)
+        self.assertEqual(len(messages), 1)
+        self.assertTrue('Oops!' in str(messages[0]))
+
+    def test_fails_when_two_traits_are_already_tagged_but_archived(self):
+        """Tagging traits fails when two selected traits are already tagged with the tag but archived."""
+        already_tagged = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[0], archived=True)
+        already_tagged2 = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[1], archived=True)
+        response = self.client.post(self.get_url(self.tag.pk),
+                                    {'traits': [t.pk for t in self.traits[0:5]], })
+        self.assertEqual(response.status_code, 200)
+        messages = list(response.wsgi_request._messages)
+        self.assertEqual(len(messages), 1)
+        self.assertTrue('Oops!' in str(messages[0]))
+
+
+class ManyTaggedTraitsCreateByTagPhenotypeTaggerTest(ManyTaggedTraitsCreateByTagTestsMixin,
+                                                     PhenotypeTaggerLoginTestCase):
+
+    def setUp(self):
+        super(ManyTaggedTraitsCreateByTagPhenotypeTaggerTest, self).setUp()
+        self.tag = factories.TagFactory.create()
+        self.traits = SourceTraitFactory.create_batch(10, source_dataset__source_study_version__study=self.study)
+        self.user.refresh_from_db()
+
     def test_creates_all_new_objects_from_multiple_studies(self):
         """Correctly tags traits from two different studies in the user's taggable_studies."""
         study2 = StudyFactory.create()
@@ -1472,30 +1542,6 @@ class ManyTaggedTraitsCreateByTagTest(PhenotypeTaggerLoginTestCase):
             self.assertIn(trait, self.tag.traits.all())
             self.assertIn(self.tag, trait.tag_set.all())
 
-    def test_invalid_form_message(self):
-        """Posting invalid data results in a message about the invalidity."""
-        response = self.client.post(self.get_url(self.tag.pk), {'traits': '', })
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertTrue('Oops!' in str(messages[0]))
-
-    def test_post_blank_trait(self):
-        """Posting bad data to the form doesn't tag the trait and shows a form error."""
-        response = self.client.post(self.get_url(self.tag.pk), {'traits': '', })
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertTrue('Oops!' in str(messages[0]))
-        form = response.context['form']
-        self.assertTrue(form.has_error('traits'))
-        self.assertNotIn(self.tag, self.traits[0].tag_set.all())
-
-    def test_adds_user(self):
-        """When a trait is successfully tagged, it has the appropriate creator."""
-        response = self.client.post(self.get_url(self.tag.pk),
-                                    {'traits': [str(self.traits[0].pk)], })
-        new_object = models.TaggedTrait.objects.latest('pk')
-        self.assertEqual(self.user, new_object.creator)
-
     def test_fails_with_other_study_traits(self):
         """Tagging a trait fails when the trait is not in the user's taggable_studies'."""
         study2 = StudyFactory.create()
@@ -1503,48 +1549,6 @@ class ManyTaggedTraitsCreateByTagTest(PhenotypeTaggerLoginTestCase):
         response = self.client.post(self.get_url(self.tag.pk),
                                     {'traits': [str(x.pk) for x in traits2], 'tag': self.tag.pk, })
         # They have taggable studies and they're in the phenotype_taggers group, so view is still accessible.
-        self.assertEqual(response.status_code, 200)
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertTrue('Oops!' in str(messages[0]))
-
-    def test_fails_when_one_trait_is_already_tagged(self):
-        """Tagging traits fails when a selected trait is already tagged with the tag."""
-        already_tagged = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[0])
-        response = self.client.post(self.get_url(self.tag.pk),
-                                    {'traits': [t.pk for t in self.traits[0:5]], })
-        self.assertEqual(response.status_code, 200)
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertTrue('Oops!' in str(messages[0]))
-
-    def test_fails_when_two_traits_are_already_tagged(self):
-        """Tagging traits fails when two selected traits are already tagged with the tag."""
-        already_tagged = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[0])
-        already_tagged2 = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[1])
-        response = self.client.post(self.get_url(self.tag.pk),
-                                    {'traits': [t.pk for t in self.traits[0:5]], })
-        self.assertEqual(response.status_code, 200)
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertTrue('Oops!' in str(messages[0]))
-
-    def test_fails_when_one_trait_is_already_tagged_but_archived(self):
-        """Tagging traits fails when a selected trait is already tagged with the tag but archived."""
-        already_tagged = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[0], archived=True)
-        response = self.client.post(self.get_url(self.tag.pk),
-                                    {'traits': [t.pk for t in self.traits[0:5]], })
-        self.assertEqual(response.status_code, 200)
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertTrue('Oops!' in str(messages[0]))
-
-    def test_fails_when_two_traits_are_already_tagged_but_archived(self):
-        """Tagging traits fails when two selected traits are already tagged with the tag but archived."""
-        already_tagged = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[0], archived=True)
-        already_tagged2 = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[1], archived=True)
-        response = self.client.post(self.get_url(self.tag.pk),
-                                    {'traits': [t.pk for t in self.traits[0:5]], })
         self.assertEqual(response.status_code, 200)
         messages = list(response.wsgi_request._messages)
         self.assertEqual(len(messages), 1)
@@ -1565,99 +1569,13 @@ class ManyTaggedTraitsCreateByTagTest(PhenotypeTaggerLoginTestCase):
         self.assertEqual(response.status_code, 403)
 
 
-class ManyTaggedTraitsCreateByTagDCCAnalystTest(DCCAnalystLoginTestCase):
+class ManyTaggedTraitsCreateByTagDCCAnalystTest(ManyTaggedTraitsCreateByTagTestsMixin, DCCAnalystLoginTestCase):
 
     def setUp(self):
         super(ManyTaggedTraitsCreateByTagDCCAnalystTest, self).setUp()
         self.tag = factories.TagFactory.create()
         self.traits = SourceTraitFactory.create_batch(10, )
         self.user.refresh_from_db()
-
-    def get_url(self, *args):
-        """Get the url for the view this class is supposed to test."""
-        return reverse('tags:add-many:by-tag', args=args)
-
-    def test_view_success_code(self):
-        """View returns successful response code."""
-        response = self.client.get(self.get_url(self.tag.pk))
-        self.assertEqual(response.status_code, 200)
-
-    def test_context_data(self):
-        """View has appropriate data in the context."""
-        response = self.client.get(self.get_url(self.tag.pk))
-        context = response.context
-        self.assertTrue('form' in context)
-
-    def test_creates_new_object(self):
-        """Posting valid data to the form correctly tags a single trait."""
-        # Check on redirection to detail page, M2M links, and creation message.
-        response = self.client.post(self.get_url(self.tag.pk), {'traits': [str(self.traits[0].pk)]})
-        self.assertRedirects(response, self.tag.get_absolute_url())
-        new_object = models.TaggedTrait.objects.latest('pk')
-        self.assertIsInstance(new_object, models.TaggedTrait)
-        self.assertEqual(new_object.tag, self.tag)
-        self.assertEqual(new_object.trait, self.traits[0])
-        self.assertIn(self.traits[0], self.tag.traits.all())
-        self.assertIn(self.tag, self.traits[0].tag_set.all())
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertFalse('Oops!' in str(messages[0]))
-
-    def test_creates_two_new_objects(self):
-        """Posting valid data to the form correctly tags two traits."""
-        # Check on redirection to detail page, M2M links, and creation message.
-        some_traits = self.traits[:2]
-        response = self.client.post(self.get_url(self.tag.pk), {'traits': [str(t.pk) for t in some_traits]})
-        self.assertRedirects(response, self.tag.get_absolute_url())
-        for trait in some_traits:
-            self.assertIn(trait, self.tag.traits.all())
-            self.assertIn(self.tag, trait.tag_set.all())
-        new_objects = models.TaggedTrait.objects.all()
-        for tt in new_objects:
-            self.assertEqual(tt.tag, self.tag)
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertFalse('Oops!' in str(messages[0]))
-
-    def test_creates_all_new_objects(self):
-        """Posting valid data to the form correctly tags all of the traits listed."""
-        # Check on redirection to detail page, M2M links, and creation message.
-        response = self.client.post(self.get_url(self.tag.pk),
-                                    {'traits': [str(t.pk) for t in self.traits], 'tag': self.tag.pk, })
-        self.assertRedirects(response, self.tag.get_absolute_url())
-        for trait in self.traits:
-            self.assertIn(trait, self.tag.traits.all())
-            self.assertIn(self.tag, trait.tag_set.all())
-        new_objects = models.TaggedTrait.objects.all()
-        for tt in new_objects:
-            self.assertEqual(tt.tag, self.tag)
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertFalse('Oops!' in str(messages[0]))
-
-    def test_invalid_form_message(self):
-        """Posting invalid data results in a message about the invalidity."""
-        response = self.client.post(self.get_url(self.tag.pk), {'traits': '', })
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertTrue('Oops!' in str(messages[0]))
-
-    def test_post_blank_trait(self):
-        """Posting bad data to the form doesn't tag the trait and shows a form error."""
-        response = self.client.post(self.get_url(self.tag.pk), {'traits': '', })
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertTrue('Oops!' in str(messages[0]))
-        form = response.context['form']
-        self.assertTrue(form.has_error('traits'))
-        self.assertNotIn(self.tag, self.traits[0].tag_set.all())
-
-    def test_adds_user(self):
-        """When a trait is successfully tagged, it has the appropriate creator."""
-        response = self.client.post(self.get_url(self.tag.pk),
-                                    {'traits': [str(self.traits[0].pk)], })
-        new_object = models.TaggedTrait.objects.latest('pk')
-        self.assertEqual(self.user, new_object.creator)
 
     def test_tag_other_study_traits(self):
         """DCC user can tag traits without any taggable_studies'."""
@@ -1670,48 +1588,6 @@ class ManyTaggedTraitsCreateByTagDCCAnalystTest(DCCAnalystLoginTestCase):
         messages = list(response.wsgi_request._messages)
         self.assertEqual(len(messages), 1)
         self.assertFalse('Oops!' in str(messages[0]))
-
-    def test_fails_when_one_trait_is_already_tagged(self):
-        """Tagging traits fails when a selected trait is already tagged with the tag."""
-        already_tagged = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[0])
-        response = self.client.post(self.get_url(self.tag.pk),
-                                    {'traits': [t.pk for t in self.traits[0:5]], })
-        self.assertEqual(response.status_code, 200)
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertTrue('Oops!' in str(messages[0]))
-
-    def test_fails_when_two_traits_are_already_tagged(self):
-        """Tagging traits fails when two selected traits are already tagged with the tag."""
-        already_tagged = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[0])
-        already_tagged2 = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[1])
-        response = self.client.post(self.get_url(self.tag.pk),
-                                    {'traits': [t.pk for t in self.traits[0:5]], })
-        self.assertEqual(response.status_code, 200)
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertTrue('Oops!' in str(messages[0]))
-
-    def test_fails_when_one_trait_is_already_tagged_but_archived(self):
-        """Tagging traits fails when a selected trait is already tagged with the tag but archived."""
-        already_tagged = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[0], archived=True)
-        response = self.client.post(self.get_url(self.tag.pk),
-                                    {'traits': [t.pk for t in self.traits[0:5]], })
-        self.assertEqual(response.status_code, 200)
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertTrue('Oops!' in str(messages[0]))
-
-    def test_fails_when_two_traits_are_already_tagged_but_archived(self):
-        """Tagging traits fails when two selected traits are already tagged with the tag but archived."""
-        already_tagged = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[0], archived=True)
-        already_tagged2 = factories.TaggedTraitFactory.create(tag=self.tag, trait=self.traits[1], archived=True)
-        response = self.client.post(self.get_url(self.tag.pk),
-                                    {'traits': [t.pk for t in self.traits[0:5]], })
-        self.assertEqual(response.status_code, 200)
-        messages = list(response.wsgi_request._messages)
-        self.assertEqual(len(messages), 1)
-        self.assertTrue('Oops!' in str(messages[0]))
 
     def test_view_success_without_phenotype_taggers_group(self):
         """View is accessible even when the DCC user is not in phenotype_taggers."""
