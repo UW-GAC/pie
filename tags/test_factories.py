0ede7283b5b556e718b369e1c0eb345ab664b532
Leslie Emery <emeryl@uw.edu>
2018-10-12 11:59:14 -0700

    Add StudyResponseFactoryTest

--- tags/test_factories.py
+++ tags/test_factories.py
@@ -97,3 +97,42 @@ class DCCReviewFactoryTest(TestCase):
         instance = self.model_factory.create(status=models.DCCReview.STATUS_FOLLOWUP)
         self.assertNotEqual(instance.comment, '')
         self.assertIsInstance(instance.comment, str)
+
+
+class StudyResponseFactoryTest(TestCase):
+    model = models.StudyResponse
+    model_factory = factories.StudyResponseFactory
+
+    def test_factory_build(self):
+        """A model instance is returned by model_factory.build()."""
+        instance = self.model_factory.build()
+        self.assertIsInstance(instance, self.model)
+
+    def test_factory_create(self):
+        """A model instance is returned by model_factory.create()."""
+        instance = self.model_factory.create()
+        self.assertIsInstance(instance, self.model)
+
+    def test_factory_build_batch(self):
+        """A model instance is returned by model_factory.build_batch(5)."""
+        instances = self.model_factory.build_batch(5)
+        for one in instances:
+            self.assertIsInstance(one, self.model)
+
+    def test_factory_create_batch(self):
+        """A model instance is returned by model_factory.create_batch(5)."""
+        instances = self.model_factory.create_batch(5)
+        for one in instances:
+            self.assertIsInstance(one, self.model)
+
+    def test_status_agree(self):
+        """A comment is not created if the status is agree."""
+        instance = self.model_factory.create(status=models.StudyResponse.STATUS_AGREE)
+        self.assertEqual(instance.comment, '')
+
+    def test_status_disagree(self):
+        """A comment is created if the status is disagree."""
+        instance = self.model_factory.create(status=models.StudyResponse.STATUS_DISAGREE)
+        self.assertNotEqual(instance.comment, '')
+        self.assertIsInstance(instance.comment, str)
+
