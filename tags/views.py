04d3d7cc8cd0e860a46206008422ffcabd9f5474
Leslie Emery <emeryl@uw.edu>
2018-12-31 17:38:51 -0800

    Remove deprecated from tag x study counts
    Add filters to exclude tagged traits from deprecated SourceStudyVersions
    in counts shown on TaggedTraitTagCountsByStudy and
    TaggedTraitStudyCountsByTag.

--- tags/views.py
+++ tags/views.py
@@ -186,7 +186,9 @@ class TaggedTraitTagCountsByStudy(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(TaggedTraitTagCountsByStudy, self).get_context_data(**kwargs)
-        annotated_studies = models.TaggedTrait.objects.non_archived().values(
+        annotated_studies = models.TaggedTrait.objects.non_archived().filter(
+            trait__source_dataset__source_study_version__i_is_deprecated=False
+        ).values(
             study_name=F('trait__source_dataset__source_study_version__study__i_study_name'),
             study_pk=F('trait__source_dataset__source_study_version__study__pk'),
             tag_name=F('tag__title'),
@@ -207,7 +209,9 @@ class TaggedTraitStudyCountsByTag(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(TaggedTraitStudyCountsByTag, self).get_context_data(**kwargs)
-        annotated_tags = models.TaggedTrait.objects.non_archived().values(
+        annotated_tags = models.TaggedTrait.objects.non_archived().filter(
+            trait__source_dataset__source_study_version__i_is_deprecated=False
+        ).values(
             study_name=F('trait__source_dataset__source_study_version__study__i_study_name'),
             study_pk=F('trait__source_dataset__source_study_version__study__pk'),
             tag_name=F('tag__title'),
