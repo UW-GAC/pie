89ae277f5ba1357580d78c3527f26200686308a6
Leslie Emery <emeryl@uw.edu>
2020-01-03 23:52:00 -0800

    Update built documentation for deployment

--- docs/_static/bizstyle.js
+++ docs/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("PIE 2.13 documentation");
+        $("li.nav-item-0 a").text("PIE 2.14 documentation");
     }
 });
